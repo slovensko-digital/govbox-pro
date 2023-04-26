2d3847ba080db234188256341b6f105a5b566abe
luciajanikova <19lucia99@gmail.com>
2023-04-26 15:12:29 +0200

    Add associations to models

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -8,4 +8,6 @@
 #  updated_at                                  :datetime         not null
 
 class Tenant < ApplicationRecord
+  has_many :subjects
+  has_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,15 @@
 #  id                                          :integer          not null, primary key
 #  email                                       :string           not null
 #  name                                        :string           not null
+#  tenant_id                                   :integer
 #  role                                        :integer          default("regular"), not null
 #  created_at                                  :datetime         not null
 #  updated_at                                  :datetime         not null
 
 class User < ApplicationRecord
   enum role: { regular: 0, admin: 1, super_admin: 2 }
+
+  belongs_to :tenant
 
   def logged_in?
     true
