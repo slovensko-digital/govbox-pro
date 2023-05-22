c987a6ec849df5f8684407c225fb06d0f8e73c98
stage-rl <robert.lences@gmail.com>
2023-05-22 12:55:24 +0200

    seed.rb for first tenant and SITE_ADMIN

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-@tenant = Tenant.create(name: 'slovensko.digital')
+puts 'Type First tenant name'
+tenant_name = STDIN.gets.chomp
+@tenant = Tenant.create(name: tenant_name)
 @group_all = @tenant.groups.create(name: 'All Tenant users - default system group', group_type: 'ALL')
 @group_admin = @tenant.groups.create(name: 'Tenant admins - default system group', group_type: 'ADMIN')
 
-@user = @tenant.users.create(email: 'robert.lences@gmail.com', name: 'Robo Lences', user_type: 'SITE_ADMIN')
+puts 'Type SITE ADMIN name'
+site_admin_name = STDIN.gets.chomp
+puts 'Type SITE ADMIN gmail address - used for authentication'
+site_admin_email = STDIN.gets.chomp
+@user = @tenant.users.create(email: site_admin_email, name: site_admin_name, user_type: 'SITE_ADMIN')
 @group_user = @user.groups.create(name: @user.name + ' group - default system group', group_type: 'USER', tenant_id: @user.tenant_id)
