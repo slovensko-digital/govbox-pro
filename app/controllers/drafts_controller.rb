c237436fc5effb1d565e49bf64f51285e394a147
luciajanikova <19lucia99@gmail.com>
2023-05-24 09:10:56 +0200

    Refactor DraftsController

--- app/controllers/drafts_controller.rb
+++ app/controllers/drafts_controller.rb
@@ -1,44 +1,54 @@
 class DraftsController < ApplicationController
-  before_action :set_draft, only: [:show, :submit]
+  before_action :load_draft, only: [:show, :submit]
+  before_action :load_drafts, only: [:index, :destroy, :destroy_all, :submit_all]
+  before_action :load_drafts_imports, only: [:destroy_all]
 
   def index
-    @drafts = Current.subject.drafts
+    @drafts = @drafts.order(created_at: :desc)
   end
 
   def show
   end
 
   def destroy
-    Current.subject.drafts.find(params[:id]).destroy
+    @drafts.find(params[:id]).destroy
 
     redirect_to drafts_path
   end
 
   def destroy_all
-    Current.subject.drafts_imports.destroy_all
-    Current.subject.drafts.destroy_all
+    @drafts_imports.destroy_all
+    @drafts.destroy_all
 
     redirect_to drafts_path
   end
 
   def submit
-    @draft.being_submitted!
-
-    Drafts::SubmitJob.perform_later(@draft)
+    submit_draft(@draft)
   end
 
   def submit_all
-    @drafts = Current.subject.drafts
-
     @drafts.each do |draft|
-      draft.being_submitted!
-      Drafts::SubmitJob.perform_later(draft)
+      submit_draft(draft)
     end
   end
 
   private
 
-  def set_draft
+  def submit_draft(draft)
+    draft.being_submitted!
+    Drafts::SubmitJob.perform_later(draft)
+  end
+
+  def load_draft
     @draft = Current.subject.drafts.find(params[:id] || params[:draft_id])
+  end
+
+  def load_drafts
+    @drafts = Current.subject.drafts
+  end
+
+  def load_drafts_imports
+    @drafts_imports = Current.subject.drafts_imports
   end
 end
