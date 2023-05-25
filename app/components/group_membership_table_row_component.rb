af97eabb59e15b7dc2c6c89f03d0ede52dc41d75
stage-rl <robert.lences@gmail.com>
2023-05-25 09:28:49 +0200

    Fix indentation in Pundit policies

--- app/components/group_membership_table_row_component.rb
+++ app/components/group_membership_table_row_component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class GroupMembershipTableRowComponent < ViewComponent::Base
-    with_collection_parameter :group_membership
+  with_collection_parameter :group_membership
 
-    def initialize(group_membership:)
-        @group_membership = group_membership
-    end
+  def initialize(group_membership:)
+    @group_membership = group_membership
+  end
+
 end
