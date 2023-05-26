ca66e2808b6215581c7d640f34b606058ba19ec7
Jan Suchal <jan.suchal@gmail.com>
2023-05-26 20:01:11 +0200

    Fix seeds for tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,8 @@
 #   movies = Movie.find_or_create_by([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.find_or_create_by(name: 'Luke', movie: movies.first)
 
-# TODO - upratat, aby sa nic nepytalo
-puts 'Type First tenant name'
-tenant_name = STDIN.gets.chomp
-@tenant = Tenant.find_or_create_by!(name: tenant_name)
+tenant = Tenant.find_or_create_by!(name: 'Dummy Tenant')
 
-puts 'Type ADMIN name'
-site_admin_name = STDIN.gets.chomp
-puts 'Type ADMIN gmail address - used for authentication'
-site_admin_email = STDIN.gets.chomp
-@user = @tenant.users.find_or_create_by!(email: site_admin_email, name: site_admin_name)
-
+ENV['SITE_ADMIN_EMAILS'].to_s.split(',').each.with_index(1) do |email, i|
+  tenant.users.find_or_create_by!(email: email, name: "Site ADMIN User #{i}")
+end
