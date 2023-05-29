b365e8d7dc9dc491c8af91d466162bbb4d220ac9
stage-rl <robert.lences@gmail.com>
2023-05-29 14:24:31 +0200

    Move administration to /admin/ namespace

--- app/controllers/admin/tenants_controller.rb
+++ app/controllers/admin/tenants_controller.rb
@@ -1,4 +1,4 @@
-class TenantsController < ApplicationController
+class Admin::TenantsController < ApplicationController
   before_action :set_tenant, only: %i[show edit update destroy]
 
   def index
@@ -27,7 +27,7 @@ class TenantsController < ApplicationController
     authorize @tenant
     respond_to do |format|
       if @tenant.save
-        format.html { redirect_to tenant_url(@tenant), notice: 'Tenant was successfully created.' }
+        format.html { redirect_to admin_tenant_url(@tenant), notice: 'Tenant was successfully created.' }
         format.json { render :show, status: :created, location: @tenant }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -41,7 +41,7 @@ class TenantsController < ApplicationController
     authorize @tenant
     respond_to do |format|
       if @tenant.update(tenant_params)
-        format.html { redirect_to tenant_url(@tenant), notice: 'Tenant was successfully updated.' }
+        format.html { redirect_to admin_tenant_url(@tenant), notice: 'Tenant was successfully updated.' }
         format.json { render :show, status: :ok, location: @tenant }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -56,7 +56,7 @@ class TenantsController < ApplicationController
     @tenant.destroy
     session[:tenant_id] = nil
     respond_to do |format|
-      format.html { redirect_to tenants_url, notice: 'Tenant was successfully destroyed.' }
+      format.html { redirect_to admin_tenants_url, notice: 'Tenant was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
