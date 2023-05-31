07963d94fc976a144c1d336c3f4dc6a81f2aa6b9
Jan Suchal <jan.suchal@gmail.com>
2023-05-31 22:35:23 +0200

    Refactor a bit

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,5 @@ ENV['SITE_ADMIN_EMAILS'].to_s.split(',').each.with_index(1) do |email, i|
   tenant.users.find_or_create_by!(email: email, name: "Site ADMIN User #{i}")
 end
 
-box = Box.create!(name: "Dev box", uri: "ico://sk/83300252", tenant_id: tenant.id)
-Govbox::ApiConnection.create!(sub: "SPL_Irvin_83300252_KK_24022023", box_id: box.id, api_token_private_key: File.read(Rails.root + "security/govbox_api_fix.pem"))
+box = tenant.boxes.create!(name: "Dev box", uri: "ico://sk/83300252")
+Govbox::ApiConnection.create!(sub: "SPL_Irvin_83300252_KK_24022023", box: box, api_token_private_key: File.read(Rails.root + "security/govbox_api_fix.pem"))
