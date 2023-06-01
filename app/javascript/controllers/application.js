4f4aed5f33e93476200810fb0e0df36b44e95d8a
stage-rl <robert.lences@gmail.com>
2023-06-01 08:57:51 +0200

    Add stimulus, jsbundling, tailwindcss-stimulus-components (yarn)

---
+++ app/javascript/controllers/application.js
@@ -0,0 +1,20 @@
+import { Application } from "@hotwired/stimulus"
+
+const application = Application.start()
+
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus   = application
+
+// Import and register all TailwindCSS Components
+import { Alert, Autosave, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from "tailwindcss-stimulus-components"
+application.register('alert', Alert)
+application.register('autosave', Autosave)
+application.register('dropdown', Dropdown)
+application.register('modal', Modal)
+application.register('tabs', Tabs)
+application.register('popover', Popover)
+application.register('toggle', Toggle)
+application.register('slideover', Slideover)
+
+export { application }
