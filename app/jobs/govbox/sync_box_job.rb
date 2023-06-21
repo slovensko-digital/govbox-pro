f6359a148fd91dab4b627c26c8bc98eb6316d981
luciajanikova <19lucia99@gmail.com>
2023-06-21 09:52:30 +0200

    Update creating folders IV

--- app/jobs/govbox/sync_box_job.rb
+++ app/jobs/govbox/sync_box_job.rb
@@ -4,33 +4,36 @@ module Govbox
 
     def perform(box, upvs_client: UpvsEnvironment.upvs_client)
       edesk_api = upvs_client.api(box).edesk
-      response_status, folders = edesk_api.fetch_folders
+      response_status, raw_folders = edesk_api.fetch_folders
 
       raise "Unable to fetch folders" if response_status != 200
 
-      find_or_create_folder_with_parent(folders, box)
+      raw_folders = raw_folders.index_by {|f| f["id"]}
+      raw_folders.each do |folder_hash|
+        folder = find_or_create_folder_with_parent(folder_hash[1], folders, box)
+        SyncFolderJob.perform_later(folder)
+      end
     end
 
     private
 
-    def find_or_create_folder_with_parent(folders, box)
-      return if folders.empty?
+    def find_or_create_folder_with_parent(folder_hash, folders, box)
+      parent_folder = nil
 
-      folder_hash = folders.pop
+      if folder_hash['parent_id']
+        parent_folder = find_or_create_folder_with_parent(folders[folder_hash['parent_id']], folders, box)
+      end
 
       folder = Govbox::Folder.find_or_initialize_by(edesk_folder_id: folder_hash['id']).tap do |f|
         f.edesk_folder_id = folder_hash['id']
+        f.parent_folder_id = parent_folder&.id
         f.name = folder_hash['name']
         f.system = folder_hash['system'] || false
         f.box = box
         f.save!
       end
 
-      find_or_create_folder_with_parent(folders, box)
-
-      folder.update!(parent_folder_id: Govbox::Folder.find_by(edesk_folder_id: folder_hash['parent_id'])&.id)
-
-      SyncFolderJob.perform_later(folder)
+      folder
     end
   end
 end
