00d5c98b8ba62d9ef9a83c12c0f7b2b5f1edc892
stage-rl <robert.lences@gmail.com>
2023-07-20 14:33:39 +0200

    Fix view "Bez stitku"

--- app/controllers/message_threads_controller.rb
+++ app/controllers/message_threads_controller.rb
@@ -16,23 +16,22 @@ class MessageThreadsController < ApplicationController
 
   def index
     authorize MessageThread
-    @delivered_at = 'message_threads.delivered_at'
-    @id = 'message_threads.id'
     @cursor = params[:cursor] || {}
-    @cursor[@delivered_at] = @cursor[@delivered_at] ? millis_to_time(@cursor[@delivered_at]) : Time.now
+    @cursor[DELIVERED_AT] = @cursor[DELIVERED_AT] ? millis_to_time(@cursor[DELIVERED_AT]) : Time.now
 
     @message_threads, @next_cursor =
       Pagination.paginate(
         collection: message_threads_collection,
         cursor: {
-          @delivered_at => @cursor[@delivered_at],
-          @id => @cursor[@id]
+          DELIVERED_AT => @cursor[DELIVERED_AT],
+          ID => @cursor[ID]
         },
         items_per_page: MESSAGE_THREADS_PER_PAGE,
         direction: 'desc'
       )
 
-    @next_cursor[@delivered_at] = time_to_millis(@next_cursor[@delivered_at]) if @next_cursor
+    @next_cursor[DELIVERED_AT] = time_to_millis(@next_cursor[DELIVERED_AT]) if @next_cursor
+    @next_page_params = message_thread_params.merge(cursor: @next_cursor).merge(format: :turbo_stream)
 
     respond_to do |format|
       format.html # GET
@@ -43,7 +42,7 @@ class MessageThreadsController < ApplicationController
   # TODO: presunut logiku do modelu
   def merge
     authorize MessageThread
-    @selected_message_threads = policy_scope(MessageThread).where(id: params[:message_thread_ids]).order(:delivered_at)
+    @selected_message_threads = policy_scope(MessageThread).where(ID: params[:message_thread_ids]).order(:delivered_at)
     if !@selected_message_threads || @selected_message_threads.size < 2
       flash[:error] = 'Označte zaškrtávacími políčkami minimálne 2 vlákna, ktoré chcete spojiť'
       redirect_back fallback_location: message_threads_path
@@ -53,7 +52,7 @@ class MessageThreadsController < ApplicationController
     MessageThread.transaction do
       @selected_message_threads.each_with_index do |thread, i|
         if i.positive?
-          @target_thread.merge_uuids.union(thread.merge_uuids)
+          @target_thread.merge_uuIDs.union(thread.merge_uuIDs)
           thread.messages.each do |message|
             message.thread = @target_thread
             message.save!
@@ -69,15 +68,17 @@ class MessageThreadsController < ApplicationController
 
   private
 
-  MESSAGE_THREADS_PER_PAGE = 10
+  MESSAGE_THREADS_PER_PAGE = 20
+  DELIVERED_AT = 'message_threads.delivered_at'
+  ID = 'message_threads.id'
 
   def message_threads_collection
     @message_threads_collection = policy_scope(MessageThread).joins(:tags).includes(:tags)
     @message_threads_collection = @message_threads_collection.where(tags: { id: params[:tag_id] }) if params[:tag_id]
-    @message_threads_collection =
-      @message_threads_collection
-        .where.not(message_threads:
-          {id: (MessageThread.joins(:tags).where(tags: {visible: true}))})  if params[:tags] && params[:tags] == 'none' && Current.user.admin?
+    if params[:tags] && params[:tags] == 'none' && Current.user.admin?
+      @message_threads_collection =
+        @message_threads_collection.where.not(message_threads: { id: (MessageThread.joins(:tags).where('tags.name not like ?', 'slovensko.sk:%')) })
+    end
     add_calculated_fields
   end
 
@@ -85,7 +86,7 @@ class MessageThreadsController < ApplicationController
     @message_threads_collection.select(
       'message_threads.*',
       'tags.*',
-      # TODO - mame tu velmi hruby sposob ako zistit, s kym je dany thread komunikacie, vedeny, len pre ucely zobrazenia. Dohodnut aj s @Taja, co s tym
+      # TODO: - mame tu velmi hruby sposob ako zistit, s kym je dany thread komunikacie, vedeny, len pre ucely zobrazenia. Dohodnut aj s @Taja, co s tym
       '(select count(messages.id) from messages where messages.message_thread_id = message_threads.id) as messages_count,
       coalesce((select max(coalesce(recipient_name)) from messages where messages.message_thread_id = message_threads.id),
         (select max(coalesce(sender_name)) from messages where messages.message_thread_id = message_threads.id)) as with_whom',
@@ -107,6 +108,6 @@ class MessageThreadsController < ApplicationController
   end
 
   def message_thread_params
-    params.require(:message_thread).permit(:title, :original_title, :merge_uuids, :tags)
+    params.permit(:message_thread, :title, :original_title, :merge_uuIDs, :tag_ID, :tags, :format, cursor: [DELIVERED_AT, ID])
   end
 end
