ecd34fddba7ed422f0cd385c039aec7edc472479
luciajanikova <19lucia99@gmail.com>
2023-07-27 17:17:27 +0200

    Change migration to update existing records

--- db/migrate/20230721142114_add_last_message_delivered_at_to_message_threads.rb
+++ db/migrate/20230721142114_add_last_message_delivered_at_to_message_threads.rb
@@ -1,5 +1,14 @@
 class AddLastMessageDeliveredAtToMessageThreads < ActiveRecord::Migration[7.0]
   def change
-    add_column :message_threads, :last_message_delivered_at, :datetime, null: false
+    add_column :message_threads, :last_message_delivered_at, :datetime
+
+    MessageThread.find_each do |message_thread|
+      last_message_delivered_at = message_thread.messages.order(:delivered_at).last.delivered_at
+      message_thread.update(
+        last_message_delivered_at: last_message_delivered_at
+      )
+    end
+
+    change_column :message_threads, :last_message_delivered_at, :datetime, null: false
   end
 end
