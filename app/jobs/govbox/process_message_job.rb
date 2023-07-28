74ce944f9d2a0084c53ff127668c8e62a5265c09
luciajanikova <19lucia99@gmail.com>
2023-07-28 08:20:11 +0200

    Mark delivery notification with existing message as authorized

--- app/jobs/govbox/process_message_job.rb
+++ app/jobs/govbox/process_message_job.rb
@@ -8,6 +8,15 @@ module Govbox
       ActiveRecord::Base.transaction do
         Govbox::Message.create_message_with_thread!(govbox_message)
       end
+
+      # Mark message as authorized if there is a delivery notification
+      delivery_notification_govbox_message = Govbox::Message.where("payload#>>'{delivery_notification, consignment, message_id}' = ?", govbox_message.message_id).take
+
+      if delivery_notification_govbox_message
+        delivery_notification_message = ::Message.find_by(uuid: delivery_notification_govbox_message.message_id)
+        delivery_notification_message.metadata["authorized"] = true
+        delivery_notification_message.save!
+      end
     end
   end
 end
