f37f5b65499fc4c3dcba08b61c484de78cc28f68
luciajanikova <19lucia99@gmail.com>
2023-08-02 13:06:21 +0200

    Update type on existing messages

--- db/migrate/20230712092223_add_type_to_messages.rb
+++ db/migrate/20230712092223_add_type_to_messages.rb
@@ -1,5 +1,11 @@
 class AddTypeToMessages < ActiveRecord::Migration[7.0]
   def change
     add_column :messages, :type, :string
+
+    Message.find_each do |message|
+      message.update(
+        type: 'Message'
+      )
+    end
   end
 end
