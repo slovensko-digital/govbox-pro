f01d423d8a3c6febcc0a5945470c6d69cef90528
Miroslav Hettes <mirrec@gmail.com>
2023-09-14 10:08:37 +0200

    Reset column information to avoid fail in migration due AR caching

--- db/migrate/20230913122730_index_message_threads.rb
+++ db/migrate/20230913122730_index_message_threads.rb
@@ -1,5 +1,7 @@
 class IndexMessageThreads < ActiveRecord::Migration[7.0]
   def up
+    Searchable::MessageThread.reset_column_information
+
     say_with_time("Indexing MessageThreads") do
       Searchable::MessageThread.reindex_all
     end
