54f43609dd2fad5a5e9b3e8135b5bd6cc6090f0e
luciajanikova <19lucia99@gmail.com>
2023-09-18 23:21:07 +0200

    Try to add stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,7 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import AutogramSigningController from "./autogram_signing_controller"
+application.register("autogram_signing", AutogramSigningController)
+
