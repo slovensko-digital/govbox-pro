17893d1ea8349f1002795de6b92dcec8c66efacd
Miroslav Hettes <mirrec@gmail.com>
2023-09-22 17:07:38 +0200

    Index message thread with advisory_lock

--- app/jobs/searchable/reindex_message_thread_job.rb
+++ app/jobs/searchable/reindex_message_thread_job.rb
@@ -1,7 +1,13 @@
 class Searchable::ReindexMessageThreadJob < ApplicationJob
   queue_as :default
 
+  retry_on ::ApplicationRecord::FailedToAcquireLockError, wait: :exponentially_longer, attempts: Float::INFINITY
+
   def perform(message_thread)
-    ::Searchable::MessageThread.index_record(message_thread)
+    ::Searchable::MessageThread.transaction do
+      ::Searchable::MessageThread.with_advisory_lock!("mt_#{message_thread.id}", transaction: true, timeout_seconds: 10) do
+        ::Searchable::MessageThread.index_record(message_thread)
+      end
+    end
   end
 end
