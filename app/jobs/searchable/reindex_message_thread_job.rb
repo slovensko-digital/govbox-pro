2bf7ac658ef74ee8bfe2b359695d4864e092f2e6
Miroslav Hettes <mirrec@gmail.com>
2023-09-22 17:29:56 +0200

    Limit total number of unfinished jobs for reindexing

--- app/jobs/searchable/reindex_message_thread_job.rb
+++ app/jobs/searchable/reindex_message_thread_job.rb
@@ -1,6 +1,16 @@
 class Searchable::ReindexMessageThreadJob < ApplicationJob
   queue_as :default
 
+  include GoodJob::ActiveJobExtensions::Concurrency
+
+  good_job_control_concurrency_with(
+    # Maximum number of unfinished jobs to allow with the concurrency key
+    # Can be an Integer or Lambda/Proc that is invoked in the context of the job
+    total_limit: 1,
+
+    key: -> { "Searchable::ReindexMessageThreadJob-#{arguments.first.id}" }
+  )
+
   retry_on ::ApplicationRecord::FailedToAcquireLockError, wait: :exponentially_longer, attempts: Float::INFINITY
 
   def perform(message_thread)
