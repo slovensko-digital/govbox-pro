56a5ea106ce7e9063c1b85134de345e290531aca
stage-rl <robert.lences@gmail.com>
2023-09-28 12:52:01 +0200

    Slight refactor

--- app/components/admin/boxes/box_form_component.rb
+++ app/components/admin/boxes/box_form_component.rb
@@ -2,5 +2,6 @@ class Admin::Boxes::BoxFormComponent < ViewComponent::Base
   def initialize(box:, action:)
     @box = box
     @action = action
+    @color_select_options = Box.colors.map { |color| [color[0], { class:"bg-#{color[0]}-100 text-#{color[0]}-600" }] }
   end
 end
