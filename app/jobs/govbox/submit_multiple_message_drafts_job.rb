eb51b95f3ab392ebb497749bdfdb6b567cb66c9d
luciajanikova <19lucia99@gmail.com>
2023-09-29 09:04:00 +0200

    Remove unused job

--- app/jobs/govbox/submit_multiple_message_drafts_job.rb
+++
@@ -1,7 +0,0 @@
-class Govbox::SubmitMultipleMessageDraftsJob < ApplicationJob
-  def perform(message_drafts, jobs_batch: GoodJob::Batch.new)
-    message_drafts.each { |message_draft| message_draft.submit(jobs_batch: jobs_batch) }
-
-    jobs_batch.enqueue(on_finish: Govbox::FinishMessageDraftsSubmitJob, box: message_drafts.first.thread.folder.box)
-  end
-end
