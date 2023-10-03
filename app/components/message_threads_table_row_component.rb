e9e717d8a15dfa7030b485d9e51ea32fc5b6258a
Miroslav Hettes <mirrec@gmail.com>
2023-10-03 21:30:27 +0200

    Display year and time only for given periods

--- app/components/message_threads_table_row_component.rb
+++ app/components/message_threads_table_row_component.rb
@@ -3,4 +3,14 @@ class MessageThreadsTableRowComponent < ViewComponent::Base
     @message_thread = message_thread
     @search_highlight = search_highlight
   end
+
+  def shorten_datetime(datetime)
+    if datetime.today?
+      l(datetime, format: "%e. %b %H:%I")
+    elsif datetime.year == Date.current.year
+      l(datetime, format: "%e. %b")
+    else
+      l(datetime, format: "%e. %b %Y")
+    end
+  end
 end
