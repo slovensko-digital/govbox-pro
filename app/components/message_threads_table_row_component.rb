ace2413c02ef4e445dc0fa19e15f1d4195c6cc6e
Miroslav Hettes <mirrec@gmail.com>
2023-10-03 22:23:16 +0200

    Move datetime formatter to view helper

--- app/components/message_threads_table_row_component.rb
+++ app/components/message_threads_table_row_component.rb
@@ -2,14 +2,4 @@ class MessageThreadsTableRowComponent < ViewComponent::Base
   def initialize(message_thread:)
     @message_thread = message_thread
   end
-
-  def shorten_datetime(datetime)
-    if datetime.today?
-      l(datetime, format: "%e. %b %H:%I")
-    elsif datetime.year == Date.current.year
-      l(datetime, format: "%e. %b")
-    else
-      l(datetime, format: "%e. %b %Y")
-    end
-  end
 end
