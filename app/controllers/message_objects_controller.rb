8cccc2dcf9bf4c1b38a5f5f7572a99f45b992afe
luciajanikova <19lucia99@gmail.com>
2023-10-05 15:10:21 +0200

    Use default object name only for FE

--- app/controllers/message_objects_controller.rb
+++ app/controllers/message_objects_controller.rb
@@ -12,12 +12,12 @@ class MessageObjectsController < ApplicationController
 
   def show
     authorize @message_object
-    send_data @message_object.message_object_datum.blob, filename: @message_object.name, type: @message_object.mimetype, disposition: :inline
+    send_data @message_object.message_object_datum.blob, filename: MessageObjectHelper.displayable_name(@message_object), type: @message_object.mimetype, disposition: :inline
   end
 
   def download
     authorize @message_object
-    send_data @message_object.message_object_datum.blob, filename: @message_object.name, type: @message_object.mimetype, disposition: :download
+    send_data @message_object.message_object_datum.blob, filename: MessageObjectHelper.displayable_name(@message_object), type: @message_object.mimetype, disposition: :download
   end
 
   def destroy
