f865b10261ddbe6074801ffaa487bf02a1d99fa4
luciajanikova <19lucia99@gmail.com>
2023-10-06 13:47:10 +0200

    Add migration to create NestedMessageObjects for existing MessageObjects

---
+++ db/migrate/20231006114400_create_nested_message_objects_from_existing_message_objects.rb
@@ -0,0 +1,18 @@
+class CreateNestedMessageObjectsFromExistingMessageObjects < ActiveRecord::Migration[7.0]
+  def change
+    MessageObject.find_each do |message_object|
+      next unless message_object.asice?
+
+      nested_message_objects = SignedAttachment::Asice.extract_documents_from_content(message_object.content)
+
+      nested_message_objects.each do |nested_message_object|
+        NestedMessageObject.find_or_create_by!(
+          name: nested_message_object.name,
+          mimetype: nested_message_object.mimetype,
+          content: nested_message_object.content,
+          parent_message_object: message_object
+        )
+      end
+    end
+  end
+end
