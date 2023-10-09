c40f8a8df7bd8a9d4001c7f9004ad59f3d398835
stage-rl <robert.lences@gmail.com>
2023-10-09 17:43:33 +0200

    Message thread sidebar cleanup

--- app/components/layout/message_thread_sidebar_component.rb
+++
@@ -1,6 +0,0 @@
-class Layout::MessageThreadSidebarComponent < ViewComponent::Base
-  def initialize(message:)
-    @selected_message = message
-    @thread_messages = @selected_message.thread.messages_visible_to_user(Current.user).order(delivered_at: :asc)
-  end
-end
