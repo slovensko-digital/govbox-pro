535350feb0747da17ab11030bb77ba51634470ce
stage-rl <robert.lences@gmail.com>
2023-10-13 12:40:45 +0200

    Add time to thread messages list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,21 @@
 module ApplicationHelper
   def nice_datetime(datetime)
     if datetime.today?
-      l(datetime, format: "%H:%M")
+      l(datetime, format: '%H:%M')
     elsif datetime.year == Date.current.year
-      l(datetime, format: "%e. %b")
+      l(datetime, format: '%e. %b')
     else
-      l(datetime, format: "%e. %b %Y")
+      l(datetime, format: '%e. %b %Y')
+    end
+  end
+
+  def nice_datetime_with_time(datetime)
+    if datetime.today?
+      l(datetime, format: '%H:%M')
+    elsif datetime.year == Date.current.year
+      l(datetime, format: '%e. %b %H:%M')
+    else
+      l(datetime, format: '%e. %b %Y %H:%M')
     end
   end
 end
