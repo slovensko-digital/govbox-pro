df74ae5e04993b8e83377531867296c03f6b475b
stage-rl <robert.lences@gmail.com>
2023-10-13 15:49:26 +0200

    Refactor based on PR review

--- app/components/message_thread_note_component.rb
+++ app/components/message_thread_note_component.rb
@@ -3,10 +3,4 @@ class MessageThreadNoteComponent < ViewComponent::Base
     @message_thread_note = message_thread_note
     @message_thread = @message_thread_note.message_thread
   end
-
-  def before_render
-    return unless @message_thread_note.updated_at
-
-    @formatted_updated_at = l @message_thread_note.updated_at, format: :long
-  end
 end
