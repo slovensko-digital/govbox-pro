49576890e1852c40c93284f71837a3ce76497b38
stage-rl <robert.lences@gmail.com>
2023-10-13 12:28:19 +0200

    Fix nice_datetime helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def nice_datetime(datetime)
     if datetime.today?
-      l(datetime, format: "%H:%I")
+      l(datetime, format: "%H:%M")
     elsif datetime.year == Date.current.year
       l(datetime, format: "%e. %b")
     else
