aa057ee24ec73bb4414663d3205a1953eefadc15
stage-rl <robert.lences@gmail.com>
2023-10-14 10:14:09 +0200

    Include notes in threads mergeing

--- app/models/message_thread.rb
+++ app/models/message_thread.rb
@@ -48,25 +48,38 @@ class MessageThread < ApplicationRecord
 
   def self.merge_threads
     transaction do
-      target_thread = self.first
-      self.all.each do |thread|
-        if thread != target_thread
-          thread.merge_identifiers.update_all(message_thread_id: target_thread.id)
-          target_thread.last_message_delivered_at = [target_thread.last_message_delivered_at, thread.last_message_delivered_at].max
-          target_thread.delivered_at = [target_thread.delivered_at, thread.delivered_at].min
-          thread.messages.each do |message|
-            message.thread = target_thread
-            message.save!
-          end
-          thread.tags.each do |tag|
-            target_thread.tags.push(tag) unless target_thread.tags.include?(tag)
-          end
-
-          thread.reload
-          thread.destroy!
-        end
+      target_thread = first
+      all.each do |thread|
+        thread.merge_thread_into(target_thread) if thread != target_thread
       end
+      target_thread.message_thread_note&.save!
       target_thread.save!
+    end
+  end
+
+  def merge_thread_into(target_thread)
+    merge_identifiers.update_all(message_thread_id: target_thread.id)
+    merge_dates(target_thread)
+    messages.update_all(message_thread_id: target_thread.id)
+    tags.each { |tag| target_thread.tags.push(tag) unless target_thread.tags.include?(tag) }
+    merge_notes(target_thread)
+    destroy!
+  end
+
+  def merge_dates(target_thread)
+    target_thread.last_message_delivered_at = [target_thread.last_message_delivered_at,
+                                               last_message_delivered_at].max
+    target_thread.delivered_at = [target_thread.delivered_at, delivered_at].min
+  end
+
+  def merge_notes(target_thread)
+    return unless message_thread_note&.note
+
+    if target_thread.message_thread_note
+      join_string = "\nPripojené z vlákna #{title} #{I18n.l DateTime.now, format: :long}:\n"
+      target_thread.message_thread_note.note << join_string << message_thread_note.note
+    else
+      target_thread.build_message_thread_note(note: message_thread_note.note)
     end
   end
 end
