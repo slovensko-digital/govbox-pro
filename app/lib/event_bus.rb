afa9143dd6b0a301d6a8f0b8ee7c3c5cc5f9770d
Miroslav Hettes <mirrec@gmail.com>
2023-10-16 18:06:03 +0200

    Use standard event names

--- app/lib/event_bus.rb
+++ app/lib/event_bus.rb
@@ -43,7 +43,10 @@ EventBus.subscribe :message_thread_changed, ->(message_thread) {
   Searchable::ReindexMessageThreadJob.perform_later(message_thread.id)
 }
 
-EventBus.subscribe :message_thread_note_committed, ->(note) {
+EventBus.subscribe :message_thread_note_created, ->(note) {
+  Searchable::ReindexMessageThreadJob.perform_later(note.message_thread_id)
+}
+EventBus.subscribe :message_thread_note_changed, ->(note) {
   Searchable::ReindexMessageThreadJob.perform_later(note.message_thread_id)
 }
 
