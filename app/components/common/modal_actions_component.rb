f57880df54ef2073ebe46e772511d0380a7f0866
Lukáš Janík <luky.janik@gmail.com>
2023-10-17 08:06:48 +0200

    Add module wrapper and hr line

--- app/components/common/modal_actions_component.rb
+++ app/components/common/modal_actions_component.rb
@@ -1,3 +1,5 @@
-class Common::ModalActionsComponent < ViewComponent::Base
-  renders_one :submit_button
+module Common
+  class ModalActionsComponent < ViewComponent::Base
+    renders_one :submit_button
+  end
 end
--- app/components/common/modal_component.rb
+++ app/components/common/modal_component.rb
@@ -1,5 +1,7 @@
-class Common::ModalComponent < ViewComponent::Base
-  renders_one :button
-  renders_one :header
-  renders_one :modal_content
+module Common
+  class ModalComponent < ViewComponent::Base
+    renders_one :button
+    renders_one :header
+    renders_one :modal_content
+  end
 end
