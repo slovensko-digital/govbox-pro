4e087c3697c8698b4c158bd0933962a1c7a6512e
luciajanikova <19lucia99@gmail.com>
2023-10-27 08:43:00 +0200

    Remove helper methods from components

--- app/components/common/alert_component.rb
+++ app/components/common/alert_component.rb
@@ -1,7 +1,4 @@
 module Common
   class AlertComponent < ViewComponent::Base
-    def notice?(type)
-      type == 'notice'
-    end
   end
 end
--- app/components/message_thread_rename_component.rb
+++ app/components/message_thread_rename_component.rb
@@ -2,8 +2,4 @@ class MessageThreadRenameComponent < ViewComponent::Base
   def initialize(message_thread:)
     @message_thread = message_thread
   end
-
-  def get_error
-    flash[:alert]
-  end
 end
