39b2456744b18482e46f03779a31a7e6bee10d32
Miroslav Hettes <mirrec@gmail.com>
2023-10-31 14:25:30 +0100

    Allow to change message_thread_tags for threads that are accessed by given user

--- app/policies/message_threads_tag_policy.rb
+++ app/policies/message_threads_tag_policy.rb
@@ -12,13 +12,23 @@ class MessageThreadsTagPolicy < ApplicationPolicy
     def resolve
       return scope.all if @user.site_admin?
 
-      scope.where(
-        Tag
-          .select(1)
-          .where("tags.id = message_threads_tags.tag_id")
-          .where(tags: { tenant_id: @user.tenant_id })
-          .arel.exists
-      )
+      # user can change tags on message_threads that he already has access to
+      scope.where(threads_accessed_by_user(@user.id))
+    end
+
+    def threads_accessed_by_user(user_id)
+      message_thread_tag_table = MessageThreadsTag.arel_table.alias('message_threads_tags_2')
+      tag_group_table = TagGroup.arel_table
+      group_membership_table = GroupMembership.arel_table
+
+      query = Arel::SelectManager.new(message_thread_tag_table)
+      query.project(1) # select 1
+      query.join(tag_group_table).on(tag_group_table[:tag_id].eq(message_thread_tag_table[:tag_id]))
+      query.join(group_membership_table).on(group_membership_table[:group_id].eq(tag_group_table[:group_id]))
+      query.where(group_membership_table[:user_id].eq(user_id))
+      query.where(Arel::Nodes::SqlLiteral.new("message_threads_tags.message_thread_id = message_threads_tags_2.message_thread_id"))
+
+      query.exists
     end
   end
 
