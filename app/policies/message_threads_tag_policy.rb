1dc644f064f6eb5abe9ee67ed7556217536cf6b6
Miroslav Hettes <mirrec@gmail.com>
2023-11-02 14:17:08 +0100

    Replace Arel with raw sql

--- app/policies/message_threads_tag_policy.rb
+++ app/policies/message_threads_tag_policy.rb
@@ -13,22 +13,16 @@ class MessageThreadsTagPolicy < ApplicationPolicy
       return scope.all if @user.site_admin?
 
       # user can change tags on message_threads that he already has access to
-      scope.where(threads_accessed_by_user(@user.id))
+      threads_accessible_by_user(scope)
     end
 
-    def threads_accessed_by_user(user_id)
-      message_thread_tag_table = MessageThreadsTag.arel_table.alias('message_threads_tags_2')
-      tag_group_table = TagGroup.arel_table
-      group_membership_table = GroupMembership.arel_table
-
-      query = Arel::SelectManager.new(message_thread_tag_table)
-      query.project(1) # select 1
-      query.join(tag_group_table).on(tag_group_table[:tag_id].eq(message_thread_tag_table[:tag_id]))
-      query.join(group_membership_table).on(group_membership_table[:group_id].eq(tag_group_table[:group_id]))
-      query.where(group_membership_table[:user_id].eq(user_id))
-      query.where(Arel::Nodes::SqlLiteral.new("message_threads_tags.message_thread_id = message_threads_tags_2.message_thread_id"))
-
-      query.exists
+    def threads_accessible_by_user(scope)
+      scope.where("EXISTS (
+        SELECT 1 FROM message_threads_tags AS message_threads_tags_2
+        INNER JOIN tag_groups ON tag_groups.tag_id = message_threads_tags_2.tag_id
+        INNER JOIN group_memberships ON group_memberships.group_id = tag_groups.group_id
+        WHERE group_memberships.user_id = ? AND message_threads_tags.message_thread_id = message_threads_tags_2.message_thread_id
+      )", @user)
     end
   end
 
