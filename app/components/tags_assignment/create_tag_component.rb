ac18210f7e376427f6352ec163e1e73ed105b66f
Miroslav Hettes <mirrec@gmail.com>
2023-11-07 00:08:38 +0100

    Move create tag component into list component

--- app/components/tags_assignment/create_tag_component.rb
+++
@@ -1,8 +0,0 @@
-module TagsAssignment
-  class CreateTagComponent < ViewComponent::Base
-    def initialize(tags_filter:, create_path:)
-      @tags_filter = tags_filter
-      @create_path = create_path
-    end
-  end
-end
--- app/components/tags_assignment/list_component.rb
+++ app/components/tags_assignment/list_component.rb
@@ -3,6 +3,7 @@ module TagsAssignment
     def initialize(tags_filter:, tags_assignments:, create_tag_path:)
       @tags_filter = tags_filter
       @tags_assignments = tags_assignments
+      @create_tag_path = create_tag_path
     end
   end
 end
