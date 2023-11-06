586f438135760d8ff906af00db52d7bbaef06fe5
Lukáš Janík <luky.janik@gmail.com>
2023-11-06 11:00:53 +0100

    Remove last_section helper

--- app/components/message_threads_table_row_component.rb
+++ app/components/message_threads_table_row_component.rb
@@ -3,8 +3,4 @@ class MessageThreadsTableRowComponent < ViewComponent::Base
     @message_thread = message_thread
     @visible_tags = message_thread.tags.select { |tag| tag.visible }
   end
-
-  def last_section?
-    @message_thread.with_whom || @message_thread.last_message_delivered_at
-  end
 end
