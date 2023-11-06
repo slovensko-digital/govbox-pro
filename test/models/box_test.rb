163ad2cedd59a4e3abf71e832e1914acb30e9ef1
luciajanikova <19lucia99@gmail.com>
2023-11-06 16:50:01 +0100

    Add tests

--- test/models/box_test.rb
+++ test/models/box_test.rb
@@ -1,4 +1,36 @@
 require "test_helper"
 
 class BoxTest < ActiveSupport::TestCase
+  test "should not be valid if not valid Govbox::ApiConnection settings" do
+    box = boxes(:one)
+    assert box.valid?
+
+    box.settings = {
+      "obo": SecureRandom.uuid
+    }
+
+    assert_equal box.valid?, false
+  end
+
+  test "should not be valid if not valid Govbox::ApiConnectionWithOboSupport settings" do
+    box = boxes(:two)
+    assert box.valid?
+
+    box.settings = {
+      "obo": SecureRandom.uuid
+    }
+
+    assert_equal box.valid?, false
+  end
+
+  test "should not be valid if not valid SkApi::ApiConnectionWithOboSupport settings" do
+    box = boxes(:three)
+    assert box.valid?
+
+    box.settings = {
+      "obo": SecureRandom.uuid
+    }
+
+    assert_equal box.valid?, false
+  end
 end
