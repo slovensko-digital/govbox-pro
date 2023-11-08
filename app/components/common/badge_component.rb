42d157b6411a43b5d57f173ba531dddfda782364
Lukáš Janík <luky.janik@gmail.com>
2023-11-08 07:54:28 +0100

    Add message state and header section components, fix responsiveness in message threads and message detail

--- app/components/common/badge_component.rb
+++ app/components/common/badge_component.rb
@@ -1,7 +1,8 @@
 module Common
   class BadgeComponent < ViewComponent::Base
-    def initialize(label)
+    def initialize(label, classes="")
       @label = label
+      @classes = classes
     end
   end
 end
