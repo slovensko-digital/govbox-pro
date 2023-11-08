1c07de8566071ab9511d4853f4def34ea1fb4e9b
Miroslav Hettes <mirrec@gmail.com>
2023-11-08 17:32:21 +0100

    Add basic implementation for bulk authorize delivery

---
+++ app/controllers/message_threads/bulk/authorize_deliveries_controller.rb
@@ -0,0 +1,24 @@
+module MessageThreads
+  module Bulk
+    class AuthorizeDeliveriesController < ::ApplicationController
+      def update
+        authorize ::Message, :authorize_delivery_notification?
+
+        ids = params[:message_thread_ids] || []
+
+        message_threads = message_thread_policy_scope.where(id: ids).includes(:messages)
+        if Govbox::BulkAuthorizedDelivery.run(message_threads)
+          redirect_back fallback_location: message_threads_path, notice: "Správy vo vláknach boli zaradené na prevzatie", status: 303
+        else
+          redirect_back fallback_location: message_threads_path, alert: "Vo vláknach sa nenašli žiadne správy na prevzatie", status: 303
+        end
+      end
+
+      private
+
+      def message_thread_policy_scope
+        policy_scope(MessageThread)
+      end
+    end
+  end
+end
