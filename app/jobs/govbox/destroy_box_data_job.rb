b107430f19e8c7612cc6c5227bcebf8d11b95783
luciajanikova <19lucia99@gmail.com>
2023-11-09 10:16:48 +0100

    Fix Govbox::DestroyBoxDataJob

--- app/jobs/govbox/destroy_box_data_job.rb
+++ app/jobs/govbox/destroy_box_data_job.rb
@@ -2,10 +2,12 @@ module Govbox
   class DestroyBoxDataJob < ApplicationJob
     queue_as :default
 
-    def perform(box_id)
-      ApiConnection.find_by(box_id: box_id)&.destroy
+    def perform(box_id, api_connection_id)
       Govbox::Folder.where(box_id: box_id).find_each { |govbox_folder| govbox_folder.messages.in_batches(of: 50).destroy_all }
-      Govbox::Folder.where(box_id: box_id).destroy_all
+
+      if Box.where(api_connection_id: api_connection_id).count == 0
+        ApiConnection.find(api_connection_id).destroy
+      end
     end
   end
 end
