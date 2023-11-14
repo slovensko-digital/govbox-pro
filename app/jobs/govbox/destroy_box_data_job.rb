ff84170d17c50eec7fb0f827937954b2404bbda5
luciajanikova <19lucia99@gmail.com>
2023-11-14 07:52:55 +0100

    Update syntax

--- app/jobs/govbox/destroy_box_data_job.rb
+++ app/jobs/govbox/destroy_box_data_job.rb
@@ -5,7 +5,7 @@ module Govbox
     def perform(box_id, api_connection_id)
       Govbox::Folder.where(box_id: box_id).find_each { |govbox_folder| govbox_folder.messages.in_batches(of: 50).destroy_all }
 
-      if Box.where(api_connection_id: api_connection_id).count == 0
+      unless Box.where(api_connection_id: api_connection_id).exists?
         api_connection = ApiConnection.find(api_connection_id)
         api_connection.destroy if api_connection.is_a?(Govbox::ApiConnection)
       end
