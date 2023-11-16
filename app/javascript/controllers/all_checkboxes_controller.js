efc4b2c939f55a1e397d3cda0955959e042682de
stage-rl <robert.lences@gmail.com>
2023-11-16 17:39:35 +0100

    Fix selected count and checkbox states

--- app/javascript/controllers/all_checkboxes_controller.js
+++ app/javascript/controllers/all_checkboxes_controller.js
@@ -1,6 +1,10 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  connect() {
+    this.update();
+  }
+
   toggle(event) {
     const form = document.getElementById("thread_checkboxes_form");
 
@@ -8,5 +12,23 @@ export default class extends Controller {
       input.checked = event.target.checked;
     });
     form.requestSubmit();
+  }
+
+  update() {
+    const form = document.getElementById("thread_checkboxes_form");
+    const checkbox_all = document.getElementById("checkbox-all");
+
+    var target_state;
+
+    form.querySelectorAll("input#message_thread_ids_").forEach((input) => {
+      if (target_state != false) {
+        if (input.checked) {
+          target_state = true;
+        } else {
+          target_state = false;
+        }
+      }
+    });
+    checkbox_all.checked = target_state;
   }
 }
