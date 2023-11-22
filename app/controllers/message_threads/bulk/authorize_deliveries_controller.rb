57300f7bd387ac1d3815af073bf15de0a8d63b79
luciajanikova <19lucia99@gmail.com>
2023-11-22 15:29:43 +0100

    Move transactions to controller & Do not force reindex during authorization

--- app/controllers/message_threads/bulk/authorize_deliveries_controller.rb
+++ app/controllers/message_threads/bulk/authorize_deliveries_controller.rb
@@ -7,10 +7,12 @@ module MessageThreads
         ids = params[:message_thread_ids] || []
 
         message_threads = message_thread_policy_scope.where(id: ids).includes(:messages)
-        if Govbox::AuthorizeDeliveryNotificationsAction.run(message_threads)
-          redirect_back fallback_location: message_threads_path, notice: "Správy vo vláknach boli zaradené na prevzatie", status: 303
-        else
-          redirect_back fallback_location: message_threads_path, alert: "Vo vláknach sa nenašli žiadne správy na prevzatie", status: 303
+        message_threads.transaction do
+          if Govbox::AuthorizeDeliveryNotificationsAction.run(message_threads)
+            redirect_back fallback_location: message_threads_path, notice: "Správy vo vláknach boli zaradené na prevzatie", status: 303
+          else
+            redirect_back fallback_location: message_threads_path, alert: "Vo vláknach sa nenašli žiadne správy na prevzatie", status: 303
+          end
         end
       end
 
--- app/models/govbox/authorize_delivery_notification_action.rb
+++ app/models/govbox/authorize_delivery_notification_action.rb
@@ -1,18 +1,15 @@
 class Govbox::AuthorizeDeliveryNotificationAction
   def self.run(message)
-    message.transaction do
-      can_be_authorized = message.can_be_authorized?
+    can_be_authorized = message.can_be_authorized?
 
-      if can_be_authorized
-        message.metadata["authorized"] = "in_progress"
-        message.save!
+    if can_be_authorized
+      message.metadata["authorized"] = "in_progress"
+      message.save!
 
-        Govbox::Message.remove_delivery_notification_tag(message)
-        Searchable::ReindexMessageThreadJob.new.perform(message.thread)
-        Govbox::AuthorizeDeliveryNotificationJob.perform_later(message)
-      end
-
-      can_be_authorized
+      Govbox::Message.remove_delivery_notification_tag(message)
+      Govbox::AuthorizeDeliveryNotificationJob.perform_later(message)
     end
+
+    can_be_authorized
   end
 end
