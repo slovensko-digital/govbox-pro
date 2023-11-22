1e86acceffe72b4dec6783795180199f1a369bd8
Miroslav Hettes <mirrec@gmail.com>
2023-11-22 10:36:25 +0100

    System test for changing tags in thread detail

---
+++ test/system/message_threads_tags_test.rb
@@ -0,0 +1,77 @@
+require "application_system_test_case"
+
+class MessageThreadsTest < ApplicationSystemTestCase
+  setup do
+    Searchable::MessageThread.reindex_all
+
+    @thread_general = message_threads(:ssd_main_general)
+
+    sign_in_as(:basic)
+  end
+
+  test "changing tags" do
+    visit message_thread_path(@thread_general)
+
+    within("#messages") do
+      within("[data-test='tags']") do
+        assert_text "Finance"
+        assert_text "Legal"
+      end
+    end
+
+    click_link "Upraviť štítky"
+
+    check "Print"
+    uncheck "Legal"
+
+    within("#tags-assignment-diff") do
+      assert_text "Print"
+      assert_text "Legal"
+    end
+
+    fill_in "name_search_query", with: "Struction"
+
+    within("#tags-assignment-list") do
+      refute_text "Legal"
+      refute_text "Print"
+    end
+
+    check "Construction"
+
+    within("#tags-assignment-diff") do
+      assert_text "Construction"
+      assert_text "Print"
+      assert_text "Legal"
+    end
+
+    check "Struction"
+
+    within("#tags-assignment-list") do
+      assert_text "Legal"
+      assert_text "Print"
+      assert_text "Struction"
+    end
+
+    within("#tags-assignment-diff") do
+      assert_text "Construction"
+      assert_text "Print"
+      assert_text "Legal"
+      assert_text "Struction"
+    end
+
+    click_button "Uložiť zmeny"
+
+    assert_text "Priradenie štítkov bolo upravené"
+
+    within("#messages") do
+      within("[data-test='tags']") do
+        assert_text "Finance"
+        assert_text "Print"
+        assert_text "Construction"
+        assert_text "Struction"
+
+        refute_text "Legal"
+      end
+    end
+  end
+end
