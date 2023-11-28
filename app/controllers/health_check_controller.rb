04a599dea9e9a47af9b9e1c13af6552101754e5b
stage-rl <robert.lences@gmail.com>
2023-11-28 20:16:30 +0100

    Simplify health check APIs

--- app/controllers/health_check_controller.rb
+++ app/controllers/health_check_controller.rb
@@ -4,9 +4,6 @@ class HealthCheckController < ApplicationController
   skip_after_action :verify_policy_scoped
   skip_before_action :set_menu_context
 
-  FETCH_LIMIT = 10
-  JOB_STUCK_MINUTES = 30
-
   def show
     if ActiveRecord::Base.connection.active?
       render status: :ok, json: { ok: true }
@@ -18,20 +15,19 @@ class HealthCheckController < ApplicationController
   end
 
   def failing_jobs
-    failed_jobs = GoodJob::Job.where.not(error: nil).limit(FETCH_LIMIT)
+    failed_jobs = GoodJob::Job.where.not(error: nil)
     if failed_jobs.any?
       render status: :service_unavailable,
-             json: { failed_jobs: failed_jobs.count == FETCH_LIMIT ? ">#{FETCH_LIMIT}" : failed_jobs.count.to_s }
+             json: { ok: false }
     else
       render status: :ok, json: { ok: true }
     end
   end
 
   def stuck_jobs
-    stuck_jobs = GoodJob::Job.where(finished_at: nil).where("COALESCE(scheduled_at, created_at) < ?", JOB_STUCK_MINUTES.minutes.ago).limit(11)
+    stuck_jobs = GoodJob::Job.where(finished_at: nil).where("COALESCE(scheduled_at, created_at) < ?", 30.minutes.ago)
     if stuck_jobs.any?
-      render status: :service_unavailable,
-             json: { stuck_jobs: stuck_jobs.count == FETCH_LIMIT ? ">#{FETCH_LIMIT}" : stuck_jobs.count.to_s }
+      render status: :service_unavailable, json: { ok: false }
     else
       render status: :ok, json: { ok: true }
     end
