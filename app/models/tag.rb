e2c8df4345c03e23b784afddd3eb3782e635b4a3
stage-rl <robert.lences@gmail.com>
2023-11-30 12:10:17 +0100

    Refactor tag action destroy error

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -23,7 +23,7 @@ class Tag < ApplicationRecord
   has_many :messages, through: :messages_tags
   has_many :message_threads_tags, dependent: :destroy
   has_many :message_threads, through: :message_threads_tags
-  has_many :automation_actions, class_name: "Automation::Action", as: :action_object
+  has_many :automation_actions, class_name: "Automation::Action", as: :action_object, dependent: :restrict_with_error
 
   validates :name, presence: true
   validates :name, uniqueness: { scope: :tenant_id, case_sensitive: false }
@@ -32,18 +32,10 @@ class Tag < ApplicationRecord
 
   after_create_commit ->(tag) { tag.mark_readable_by_groups([tag.tenant.admin_group]) }
   after_update_commit ->(tag) { EventBus.publish(:tag_renamed, tag) if previous_changes.key?("name") }
-  before_destroy :validate_tag_usage
 
   DRAFT_SYSTEM_NAME = 'draft'
 
   def mark_readable_by_groups(groups)
     self.groups += groups
-  end
-
-  def validate_tag_usage
-    return true if automation_actions.blank?
-
-    errors.add :base, "Štítok je použitý v automatizačných pravidlách, nie je ho možné vymazať. Upravte najskôr pravidlá"
-    throw :abort
   end
 end
