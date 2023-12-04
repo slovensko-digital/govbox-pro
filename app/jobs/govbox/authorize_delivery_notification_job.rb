1fe4f157c77d17adfddcaaece784471c67e0a303
stage-rl <robert.lences@gmail.com>
2023-12-04 14:39:29 +0100

    First raw version of sync authorization

--- app/jobs/govbox/authorize_delivery_notification_job.rb
+++ app/jobs/govbox/authorize_delivery_notification_job.rb
@@ -2,20 +2,25 @@ class Govbox::AuthorizeDeliveryNotificationJob < ApplicationJob
   def perform(message, upvs_client: UpvsEnvironment.upvs_client)
     edesk_api = upvs_client.api(message.thread.box).edesk
 
-    success = edesk_api.authorize_delivery_notification(message.metadata["delivery_notification"]["authorize_url"])
+    success, target_message_id = edesk_api.authorize_delivery_notification(message.metadata["delivery_notification"]["authorize_url"], mode: :sync)
 
-    unless success
-      message.metadata["authorized"] = nil
-      message.save!
-
-      Govbox::Message.add_delivery_notification_tag(message)
-
-      raise StandardError, "Delivery notification authorization failed!"
-    end
+    handle_failed_authorization unless success
 
     message.metadata["authorized"] = true
     message.save!
 
-    Govbox::SyncBoxJob.set(wait: 3.minutes).perform_later(message.thread.box)
+    raise StandardError, "Target message download failed" unless target_message_id
+    raise StandardError, "Target message download failed" unless edesk_api.fetch_message(target_message_id)
+
+    # Govbox::SyncBoxJob.set(wait: 3.minutes).perform_later(message.thread.box)
+  end
+
+  def handle_failed_authorization
+    message.metadata["authorized"] = nil
+    message.save!
+
+    Govbox::Message.add_delivery_notification_tag(message)
+
+    raise StandardError, "Delivery notification authorization failed!"
   end
 end
