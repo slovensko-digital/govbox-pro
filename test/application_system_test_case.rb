d991482814ff64a1c2e3034b23a975c459b113db
Miroslav Hettes <mirrec@gmail.com>
2023-12-06 16:21:41 +0100

    Use headless_chrome

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,7 +3,7 @@ require "helpers/auth_helper"
 require "helpers/page_parts_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driver = ENV['DRIVER'] ? ENV['DRIVER'].to_sym : :chrome
+  driver = ENV['DRIVER'] ? ENV['DRIVER'].to_sym : :headless_chrome
   driven_by :selenium, using: driver, screen_size: [1400, 1400]
 
   include AuthHelper
