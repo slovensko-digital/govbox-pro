b1497bdfc667596ab32411217caeff2792da9dfe
stage-rl <robert.lences@gmail.com>
2023-12-07 11:07:30 +0100

    Refactor based on PR review

--- app/jobs/govbox/authorize_delivery_notification_job.rb
+++ app/jobs/govbox/authorize_delivery_notification_job.rb
@@ -4,26 +4,19 @@ class Govbox::AuthorizeDeliveryNotificationJob < ApplicationJob
 
     success, target_message_id = edesk_api.authorize_delivery_notification(message.metadata["delivery_notification"]["authorize_url"], mode: :sync)
 
-    handle_failed_authorization unless success
-
-    message.metadata["authorized"] = true
-    message.save!
+    if success
+      message.metadata["authorized"] = true
+      message.save!
+    else
+      message.metadata["authorized"] = nil
+      message.save!
+      Govbox::Message.add_delivery_notification_tag(message)
+      raise StandardError, "Delivery notification authorization failed!"
+    end
 
     raise StandardError, "Target message download failed" unless target_message_id
-    raise StandardError, "Target message download failed" unless run_download_job(message, target_message_id)
+
+    Govbox::DownloadMessageJob.perform_later(box: message.thread.box, target_message_id)
   end
 
-  def handle_failed_authorization
-    message.metadata["authorized"] = nil
-    message.save!
-
-    Govbox::Message.add_delivery_notification_tag(message)
-
-    raise StandardError, "Delivery notification authorization failed!"
-  end
-
-  def run_download_job(message, message_id)
-    folder = message.thread.box.folders.select(&:inbox?).first
-    Govbox::DownloadMessageJob.perform_later(folder, message_id)
-  end
 end
--- app/jobs/govbox/download_message_job.rb
+++ app/jobs/govbox/download_message_job.rb
@@ -2,13 +2,27 @@ module Govbox
   class DownloadMessageJob < ApplicationJob
     queue_as :default
 
-    def perform(govbox_folder, edesk_message_id, upvs_client: UpvsEnvironment.upvs_client)
-      edesk_api = upvs_client.api(govbox_folder.box).edesk
-      response_status, raw_message = edesk_api.fetch_message(edesk_message_id)
+    def perform(edesk_message_id:, govbox_folder: nil, box: nil, upvs_client: UpvsEnvironment.upvs_client)
+      setup_folder_and_box(box, govbox_folder)
+
+      response_status, raw_message = upvs_client.api(@box).edesk.fetch_message(edesk_message_id)
 
       raise "Unable to fetch folder messages" if response_status != 200
 
-      govbox_message = govbox_folder.messages.create!(
+      process_govbox_message(raw_message)
+    end
+
+    def setup_folder_and_box(box, govbox_folder)
+      raise "Folder or Box must be specified" unless govbox_folder || box
+
+      @box = box || govbox_folder.box
+      @govbox_folder = govbox_folder || Govbox::Folder.where(box: box, name: 'Inbox')
+
+      raise "Folder must match specified box" unless @govbox_folder.box == box
+    end
+
+    def process_govbox_message(raw_message)
+      govbox_message = @govbox_folder.messages.create!(
         edesk_message_id: raw_message["id"],
         message_id: raw_message["message_id"],
         correlation_id: raw_message["correlation_id"],
@@ -17,7 +31,6 @@ module Govbox
         body: raw_message["original_xml"],
         payload: raw_message
       )
-
       ProcessMessageJob.perform_later(govbox_message)
     end
   end
