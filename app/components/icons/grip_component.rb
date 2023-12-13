d8e29fffaf959847baf64cd1d52a80cf8befd9ac
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2023-12-13 13:12:45 +0100

    Replace bars icon with grip icon

--- app/components/icons/grip_component.rb
+++ app/components/icons/grip_component.rb
@@ -1,4 +1,4 @@
-class Icons::BarsComponent < ViewComponent::Base
+class Icons::GripComponent < ViewComponent::Base
   def initialize(css_classes: nil, stroke_width: 1.5)
     @css_classes = css_classes
     @stroke_width = stroke_width
