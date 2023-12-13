b7cd9802a930ba883d1ac61c1b3e4eb212105884
Lukáš Janík <luky.janik@gmail.com>
2023-12-13 07:36:16 +0100

    Add initial changes according to figma

--- app/components/common/box_label_component.rb
+++ app/components/common/box_label_component.rb
@@ -1,9 +1,12 @@
 module Common
   class BoxLabelComponent < ViewComponent::Base
-    def initialize(box, classes = "", font_size_class = "text-sm")
-      @box = box
+    def initialize(box, classes="", font_size_class="text-sm", padding_classes="px-2 py-1")
       @classes = classes
       @font_size_class = font_size_class
+      @padding_classes = padding_classes
+
+      @color = box.color
+      @label = box.short_name || box.name[0]
     end
   end
 end
