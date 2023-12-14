185ed87dd18d5c4d5d3528129454b0627c68739a
Lukáš Janík <luky.janik@gmail.com>
2023-12-14 07:37:32 +0100

    Change attribute name

--- app/components/common/box_label_component.rb
+++ app/components/common/box_label_component.rb
@@ -1,9 +1,9 @@
 module Common
   class BoxLabelComponent < ViewComponent::Base
-    def initialize(box, classes="", font_size_class="text-sm", padding_classes="px-2 py-1")
+    def initialize(box, classes="", font_size_class="text-sm", padding_class="px-2 py-1")
       @classes = classes
       @font_size_class = font_size_class
-      @padding_classes = padding_classes
+      @padding_class = padding_class
 
       @color = box.color
       @label = box.short_name || box.name[0]
