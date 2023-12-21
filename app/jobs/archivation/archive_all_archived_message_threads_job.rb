27e7c6f5b4b04aef8ecf0f4d4fa8d9a2fb03b9cd
Marek Ceľuch <celuchmarek@gmail.com>
2023-12-21 09:07:08 +0100

    add todo comment and rm one turbo frame

--- app/jobs/archivation/archive_all_archived_message_threads_job.rb
+++ app/jobs/archivation/archive_all_archived_message_threads_job.rb
@@ -1,5 +1,6 @@
 class Archivation::ArchiveAllArchivedMessageThreadsJob < ApplicationJob
   def perform
+    # TODO make this more effective - do not schedule all jobs every day
     MessageThread.joins(:tags).where(tags: {type: ArchivedTag.to_s}).find_each do |message_thread|
       Archivation::ArchiveMessageThreadJob.perform_later(message_thread)
     end
