fb0d4a621e7a5309a799abebdaec8cd14c0d8a66
stage-rl <robert.lences@gmail.com>
2024-01-02 10:19:33 +0100

    Manual boxes sync

--- app/components/layout/box_selector_popup_component.rb
+++ app/components/layout/box_selector_popup_component.rb
@@ -4,5 +4,6 @@ class Layout::BoxSelectorPopupComponent < ViewComponent::Base
   def initialize(boxes_with_unread_message_counts)
     @boxes_with_unread_message_counts = boxes_with_unread_message_counts
     @all_unread_messages_count = boxes_with_unread_message_counts.values.sum
+    @next_sync_at = GoodJob::CronEntry.find("sync_boxes").next_at
   end
 end
