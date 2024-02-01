7429d58666eaeb9fdbb089895749a1d9bf6f23dd
luciajanikova <19lucia99@gmail.com>
2024-02-01 13:48:27 +0100

    Update MessageObjects mimetype

--- app/controllers/nested_message_objects_controller.rb
+++ app/controllers/nested_message_objects_controller.rb
@@ -3,12 +3,12 @@ class NestedMessageObjectsController < ApplicationController
 
   def show
     authorize @nested_message_object
-    send_data @nested_message_object.content, filename: MessageObjectHelper.displayable_name(@nested_message_object), type: @nested_message_object.mimetype || 'application/octet-stream', disposition: :inline
+    send_data @nested_message_object.content, filename: MessageObjectHelper.displayable_name(@nested_message_object), type: @nested_message_object.mimetype, disposition: :inline
   end
 
   def download
     authorize @nested_message_object
-    send_data @nested_message_object.content, filename: MessageObjectHelper.displayable_name(@nested_message_object), type: @nested_message_object.mimetype || 'application/octet-stream', disposition: :download
+    send_data @nested_message_object.content, filename: MessageObjectHelper.displayable_name(@nested_message_object), type: @nested_message_object.mimetype, disposition: :download
   end
 
   private
