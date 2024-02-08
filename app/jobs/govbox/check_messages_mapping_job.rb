494b9652e3d4813d478f238aa321eab90ca11b29
luciajanikova <19lucia99@gmail.com>
2024-02-08 19:07:08 +0100

    Update query

--- app/jobs/govbox/check_messages_mapping_job.rb
+++ app/jobs/govbox/check_messages_mapping_job.rb
@@ -3,17 +3,17 @@ module Govbox
     queue_as :default
 
     def perform
-      unmapped_govbox_messages_sql =
-      "SELECT gm.id FROM govbox_messages gm JOIN govbox_folders gf ON gm.folder_id = gf.id
-        WHERE gm.delivered_at > '#{1.months.ago}'
-        AND NOT EXISTS (
-        SELECT 1 FROM messages m
-        JOIN message_threads mt ON m.message_thread_id = mt.id
-        WHERE m.uuid = gm.message_id
-        AND mt.box_id = gf.box_id
-        LIMIT 1
-       )"
-      unmapped_govbox_messages = ActiveRecord::Base.connection.execute(unmapped_govbox_messages_sql)
+      unmapped_govbox_messages = Govbox::Message.find_by_sql(
+        ["SELECT gm.id FROM govbox_messages gm JOIN govbox_folders gf ON gm.folder_id = gf.id
+          WHERE gm.delivered_at > ?
+          AND NOT EXISTS (
+          SELECT 1 FROM messages m
+          JOIN message_threads mt ON m.message_thread_id = mt.id
+          WHERE m.uuid = gm.message_id
+          AND mt.box_id = gf.box_id
+          LIMIT 1
+         )", 1.months.ago]
+      )
 
       raise "Unmapped GovBox::Message IDs: #{unmapped_govbox_messages.pluck("id")}" if unmapped_govbox_messages.any?
     end
