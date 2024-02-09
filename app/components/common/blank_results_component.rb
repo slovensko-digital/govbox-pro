9ebf270bb29fca6c3a189fa6a65fb34c125e9206
stage-rl <robert.lences@gmail.com>
2024-02-09 14:54:10 +0100

    Changes based on PR review

--- app/components/common/blank_results_component.rb
+++ app/components/common/blank_results_component.rb
@@ -1,18 +1,22 @@
 module Common
   class BlankResultsComponent < ViewComponent::Base
-    def initialize(reason = :not_found)
-      case reason
+    def initialize (reason = :not_found)
+      @reason = reason
+    end
+
+    def before_render # rubocop:disable Metrics/MethodLength
+      case @reason
       when :empty
-        @text1 = "Žiadne záznamy"
-        @text2 = "Zatiaľ nie sú vytvorené žiadne záznamy"
-        @icon = "hand-thumb-up"
+        @text1 = t "blank_results.empty.text1"
+        @text2 = t "blank_results.empty.text2"
+        @icon = "magnifying-glass"
       when :not_found
-        @text1 = "Žiadne výsledky"
-        @text2 = "Skúste zmeniť filter alebo vyhľadávané slová"
+        @text1 = t "blank_results.not_found.text1"
+        @text2 = t "blank_results.not_found.text2"
         @icon = "magnifying-glass"
       when :all_done
-        @text1 = "Všetko je hotové"
-        @text2 = "Nemáte žiadnu novú správu"
+        @text1 = t "blank_results.all_done.text1"
+        @text2 = t "blank_results.all_done.text2"
         @icon = "hand-thumb-up"
       end
     end
