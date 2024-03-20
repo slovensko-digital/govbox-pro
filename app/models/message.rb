2cbef6b022c8eb1d9ff9433f3226249b71d7a902
luciajanikova <19lucia99@gmail.com>
2024-03-20 14:21:41 +0100

    Small update on Message, MessafeDraft

--- app/models/message.rb
+++ app/models/message.rb
@@ -106,7 +106,9 @@ class Message < ApplicationRecord
     document = Nokogiri::XML(form.unsigned_content) do |config|
       config.noblanks
     end
-    document = Nokogiri::XML(document.xpath('*:XMLDataContainer/*:XMLData/*').to_xml) if document.xpath('*:XMLDataContainer/*:XMLData').any?
+    document = Nokogiri::XML(document.xpath('*:XMLDataContainer/*:XMLData/*').to_xml) do |config|
+      config.noblanks
+    end if document.xpath('*:XMLDataContainer/*:XMLData').any?
 
     template = Nokogiri::XSLT(upvs_form.xslt_html)
     template.transform(document)
