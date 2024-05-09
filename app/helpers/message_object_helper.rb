4b4aa811c6c8bc686142684b39b3204f7a7529e3
luciajanikova <19lucia99@gmail.com>
2024-05-09 08:52:16 +0200

    Fix MessageObjectHelper.displayable_name

--- app/helpers/message_object_helper.rb
+++ app/helpers/message_object_helper.rb
@@ -1,6 +1,6 @@
 module MessageObjectHelper
   def self.displayable_name(message_object)
-    message_object.name.present? ? message_object.name : I18n.t('no_message_object_name') + Utils.file_extension_by_mime_type(message_object.mimetype)
+    message_object.name.present? ? message_object.name : I18n.t('no_message_object_name') + Utils.file_extension_by_mime_type(message_object.mimetype).to_s
   end
 
   def self.pdf_name(message_object)
