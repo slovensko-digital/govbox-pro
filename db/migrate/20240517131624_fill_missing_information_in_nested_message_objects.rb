d64f8f6f6b3600228eb7a8c055236fc2291ee9c3
luciajanikova <19lucia99@gmail.com>
2024-05-20 10:04:23 +0200

    Update FillMissingInformationInNestedMessageObjects migration

--- db/migrate/20240517131624_fill_missing_information_in_nested_message_objects.rb
+++ db/migrate/20240517131624_fill_missing_information_in_nested_message_objects.rb
@@ -2,12 +2,17 @@ class FillMissingInformationInNestedMessageObjects < ActiveRecord::Migration[7.1
   def up
     NestedMessageObject.where(mimetype: 'application/octet-stream').find_each do |nested_message_object|
       manifest_file_content = ::SignedAttachment::Asice.get_manifest_file_content(nested_message_object.message_object.content)
-      xml_manifest = Nokogiri::XML(manifest_file_content) if manifest_file_content
+
+      next unless manifest_file_content.present?
+
+      xml_manifest = Nokogiri::XML(manifest_file_content)
 
       mimetype_from_manifest = xml_manifest.xpath("//manifest:file-entry[@manifest:full-path = '#{nested_message_object.name}']/@manifest:media-type")&.first&.value
 
-      nested_message_object.mimetype = mimetype_from_manifest if mimetype_from_manifest
-      nested_message_object.name += Utils.file_extension_by_mime_type(nested_message_object.mimetype).to_s if nested_message_object.name.present? && !nested_message_object.name&.include?(Utils.file_extension_by_mime_type(nested_message_object.mimetype).to_s)
+      next unless mimetype_from_manifest.present?
+
+      nested_message_object.mimetype = mimetype_from_manifest
+      nested_message_object.name += Utils.file_extension_by_mime_type(nested_message_object.mimetype).to_s if Utils.file_name_without_extension?(nested_message_object))
 
       nested_message_object.save
     end
