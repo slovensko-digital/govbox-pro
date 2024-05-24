5116c380276ceea7909b5c8c44db160ba72afb72
luciajanikova <19lucia99@gmail.com>
2024-05-24 10:04:02 +0200

    Remove temporary jobs

--- app/jobs/update_message_objects_name_job.rb
+++
@@ -1,9 +0,0 @@
-class UpdateMessageObjectsNameJob < ApplicationJob
-  def perform
-    MessageObject.find_each do |message_object|
-      next unless message_object.name.present?
-
-      UpdateMessageObjectNameJob.perform_later(message_object)
-    end
-  end
-end
