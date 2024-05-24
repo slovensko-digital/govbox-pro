f4093c044bd68ea82bdf51d74cbef8779dd2f537
luciajanikova <19lucia99@gmail.com>
2024-05-24 15:32:09 +0200

    Update FS file naming convention

--- app/lib/utils/fs_information_parser.rb
+++ app/lib/utils/fs_information_parser.rb
@@ -1,6 +1,6 @@
 module Utils
   class FsInformationParser
-    FILE_NAME_REGEXP = /dic(\d+)_fs[a-zA-Z0-9_-]+?__/
+    FILE_NAME_REGEXP = /dic(\d+)_fs([a-zA-Z0-9_-]+?)__/
 
     def self.parse_info_from_filename(filename)
       FILE_NAME_REGEXP.match(filename)&.captures
