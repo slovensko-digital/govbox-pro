30577f03ecc9c6f386cc39078529a4250cf00fb4
luciajanikova <19lucia99@gmail.com>
2024-05-28 11:32:00 +0200

    Update Fs::MessageDraft visualization

--- app/helpers/message_helper.rb
+++ app/helpers/message_helper.rb
@@ -2,4 +2,10 @@ module MessageHelper
   def self.export_filename(message)
     "#{message.delivered_at.to_date}-sprava-#{message.id}.zip"
   end
+
+  def self.format_html_visualization(message)
+    return ActionController::Base.helpers.simple_format(message.html_visualization) if message.is_a?(Fs::MessageDraft)
+
+    message.html_visualization
+  end
 end
