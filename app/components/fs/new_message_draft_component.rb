fbdae4f7edd83c5ba37f37334920e55c2323728e
luciajanikova <19lucia99@gmail.com>
2024-06-17 10:09:50 +0200

    Remove extra view component

--- app/components/fs/new_message_draft_component.rb
+++
@@ -1,7 +0,0 @@
-class Fs::NewMessageDraftComponent < ViewComponent::Base
-  DEFAULT_CLASSES = 'bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500'
-  ERROR_CLASSES = 'bg-red-50 border border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500'
-
-  def initialize
-  end
-end
