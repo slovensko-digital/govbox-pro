a1e92d6c9559925014d65a4429c2d911fa7b25d4
luciajanikova <19lucia99@gmail.com>
2024-07-02 10:40:51 +0200

    Add search by UUID to messages API

--- test/integration/messages_api_test.rb
+++ test/integration/messages_api_test.rb
@@ -45,6 +45,29 @@ class ThreadsApiTest < ActionDispatch::IntegrationTest
     assert_response :not_found
   end
 
+  test "can search message by UUID" do
+    message = messages(:ssd_main_general_one)
+
+    get "/api/messages/search", params: { uuid: message.uuid, token: generate_api_token(sub: @tenant.id, key_pair: @key_pair) }, as: :json
+
+    assert_response :success
+    json_response = JSON.parse(response.body)
+    assert_equal message.id, json_response["id"]
+    assert_equal message.uuid, json_response["uuid"]
+    assert_equal message.title, json_response["title"]
+    assert_equal message.sender_name, json_response["sender_name"]
+    assert_equal message.recipient_name, json_response["recipient_name"]
+    assert_equal message.delivered_at, Time.zone.parse(json_response["delivered_at"])
+    message.objects.each do |object|
+      assert_equal object.name, json_response["objects"][0]["name"]
+      assert_equal object.mimetype, json_response["objects"][0]["mimetype"]
+      assert_equal object.object_type, json_response["objects"][0]["object_type"]
+      assert_in_delta object.updated_at, Time.zone.parse(json_response["objects"][0]["updated_at"]), 0.001
+      assert_equal object.is_signed, json_response["objects"][0]["is_signed"]
+      assert_equal object.message_object_datum.blob, Base64.decode64(json_response["objects"][0]["data"])
+    end
+  end
+
   test "can sync messages" do
     get "/api/messages/sync", params: { token: generate_api_token(sub: @tenant.id, key_pair: @key_pair) }, as: :json
 
