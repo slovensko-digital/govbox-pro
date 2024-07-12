be81e6144cd1f0acabd596a688bf2fb9afab893c
luciajanikova <19lucia99@gmail.com>
2024-07-12 12:24:10 +0200

    Update test

--- test/models/fs/message_draft_test.rb
+++ test/models/fs/message_draft_test.rb
@@ -7,8 +7,17 @@ class Fs::MessageDraftTest < ActiveSupport::TestCase
   include ActionDispatch::TestProcess::FixtureFile
 
   test "create_and_validate_with_fs_form method schedules Fs::ValidateMessageDraftJob" do
+    fs_api = Minitest::Mock.new
+    fs_api.expect :parse_form, {
+      "subject" => "1122334455",
+      "form_identifier" => "3055_781"
+    },
+    [Base64.strict_encode64(file_fixture("fs/dic1122334455_fs3055_781__sprava_dani_2023.xml").read)]
+
+  FsEnvironment.fs_client.stub :api, fs_api do
     assert_enqueued_with(job: Fs::ValidateMessageDraftJob) do
-      Fs::MessageDraft.create_and_validate_with_fs_form(form_files: [fixture_file_upload("fs/dic1122334455_fs3055_781__sprava_dani_2023.xml", "application/xml")], author: users(:accountants_basic))
+        Fs::MessageDraft.create_and_validate_with_fs_form(form_files: [fixture_file_upload("fs/dic1122334455_fs3055_781__sprava_dani_2023.xml", "application/xml")], author: users(:accountants_basic))
+      end
     end
   end
 end
