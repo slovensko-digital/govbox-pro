813ec4523645c7c86b0450a38e554ad0b9dbb147
luciajanikova <19lucia99@gmail.com>
2024-08-22 18:44:45 +0200

    Add test

--- test/system/message_drafts_test.rb
+++ test/system/message_drafts_test.rb
@@ -1,6 +1,22 @@
 require "application_system_test_case"
 
 class MessageDraftsTest < ApplicationSystemTestCase
+  include ActiveJob::TestHelper
+
+  setup do
+    sign_in_as(:basic)
+  end
+
   test "user can create message draft as reply on replyable message" do
+  end
+
+  test "single draft submission schedules jobs with highest priority" do
+    message_draft = messages(:ssd_main_draft)
+
+    visit message_thread_path(message_draft.thread)
+
+    assert_enqueued_with(job: Govbox::SubmitMessageDraftJob, queue: :highest_priority) do
+      click_button "Odoslať"
+    end
   end
 end
