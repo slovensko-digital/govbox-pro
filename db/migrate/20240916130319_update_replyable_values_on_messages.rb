8d35575a5af755b91f2a2703f41be2c1167bfbdb
luciajanikova <19lucia99@gmail.com>
2024-09-16 13:11:10 +0200

    Update message replyable? rules

---
+++ db/migrate/20240916130319_update_replyable_values_on_messages.rb
@@ -0,0 +1,8 @@
+class UpdateReplyableValuesOnMessages < ActiveRecord::Migration[7.0]
+  def change
+    Message.find_each do |message|
+      govbox_message = Govbox::Message.find_by(message_id: message.uuid)
+      message.update(replyable: govbox_message&.replyable? || false)
+    end
+  end
+end
