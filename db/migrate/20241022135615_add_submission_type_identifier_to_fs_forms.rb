63ce7b3195eb5411f8c1a6f16471cac39a4bd47c
luciajanikova <19lucia99@gmail.com>
2024-10-25 13:23:32 +0200

    Schedule Fs::FetchFormsJob in latest migration

--- db/migrate/20241022135615_add_submission_type_identifier_to_fs_forms.rb
+++ db/migrate/20241022135615_add_submission_type_identifier_to_fs_forms.rb
@@ -1,5 +1,7 @@
 class AddSubmissionTypeIdentifierToFsForms < ActiveRecord::Migration[7.1]
   def change
     add_column :fs_forms, :submission_type_identifier, :string
+
+    Fs::FetchFormsJob.perform_later
   end
 end
