9de913524eb091b7926a4e1a2a7b17aec35ce1e6
luciajanikova <19lucia99@gmail.com>
2024-11-12 13:29:26 +0100

    Update automation to be able to add tag to thread after form object is downloaded

--- app/components/settings/rules/rule_header_form_component.rb
+++ app/components/settings/rules/rule_header_form_component.rb
@@ -6,6 +6,7 @@ class Settings::Rules::RuleHeaderFormComponent < ViewComponent::Base
     @trigger_events_list = [
       [t('message_created'), 'message_created'],
       [t('message_draft_submitted'), 'message_draft_submitted'],
+      [t('form_object_downloaded'), 'form_object_downloaded'],
     ]
   end
 end
