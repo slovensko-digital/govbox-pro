5868114353ab69302bf088d4c1d7ef34fbf2ee17
luciajanikova <19lucia99@gmail.com>
2024-11-14 12:16:58 +0100

    Update syntax

--- app/jobs/fs/validate_message_draft_result_job.rb
+++ app/jobs/fs/validate_message_draft_result_job.rb
@@ -18,17 +18,11 @@ class Fs::ValidateMessageDraftResultJob < ApplicationJob
       }
 
       diff = response[:body]['problems']&.select { |problem| problem['level'] == 'diff' }
-      log("Message draft DIFF: #{diff.map{ |problem| problem['message']}.join(', ')}") if diff.any?
+      Rails.logger.info("Message draft DIFF: #{diff.map{ |problem| problem['message']}.join(', ')}") if diff.any?
 
       message_draft.add_cascading_tag(message_draft.tenant.submission_error_tag) if message_draft.metadata[:validation_errors][:errors].any? || message_draft.metadata[:validation_errors][:warnings].any?
     end
 
     message_draft.save
-  end
-
-  private
-
-  def log(message)
-    Rails.logger.info(message)
   end
 end
