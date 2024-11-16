ddbce3e5b65f37edbd8ea8cfe0e0d6b46ed8b7bf
Michal Barla <michal.barla@gmail.com>
2024-11-16 18:09:23 +0100

    Get rid of thumbs up, separate filters, rules & tags

--- app/components/common/blank_results_component.rb
+++ app/components/common/blank_results_component.rb
@@ -6,18 +6,22 @@ module Common
 
     def before_render # rubocop:disable Metrics/MethodLength
       case @reason
-      when :empty
-        @text1 = t "blank_results.empty.text1"
-        @text2 = t "blank_results.empty.text2"
-        @icon = "magnifying-glass"
+      when :filters
+        @text1 = t "blank_results.filters.text1"
+        @text2 = t "blank_results.filters.text2"
+        @icon = "bookmark-slash"
+      when :rules
+        @text1 = t "blank_results.rules.text1"
+        @text2 = t "blank_results.rules.text2"
+        @icon = "funnel-slash"
+      when :tags
+        @text1 = t "blank_results.tags.text1"
+        @text2 = t "blank_results.tags.text2"
+        @icon = "tag-slash"
       when :not_found
         @text1 = t "blank_results.not_found.text1"
         @text2 = t "blank_results.not_found.text2"
         @icon = "magnifying-glass"
-      when :all_done
-        @text1 = t "blank_results.all_done.text1"
-        @text2 = t "blank_results.all_done.text2"
-        @icon = "hand-thumb-up"
       end
     end
   end
