f1979cb25cbd34f4298b0054b0c43c7771852db3
Jan Suchal <jan.suchal@gmail.com>
2024-11-26 10:24:42 +0100

    Migrate to importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,3 @@
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from "app/javascript/custom", under: "custom"
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
