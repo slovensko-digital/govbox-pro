9fa07807cc91617ff86b8561eba87f93fec88d45
luciajanikova <19lucia99@gmail.com>
2024-12-03 09:07:26 +0100

    Do not retry Fs::DownloadSentMessageRelatedMessagesJob if NoRelatedMessagesError

--- app/jobs/fs/download_sent_message_related_messages_job.rb
+++ app/jobs/fs/download_sent_message_related_messages_job.rb
@@ -1,5 +1,12 @@
 module Fs
   class DownloadSentMessageRelatedMessagesJob < ApplicationJob
+    class NoRelatedMessagesError < StandardError
+    end
+
+    retry_on NoRelatedMessagesError, attempts: 1 do |_job, _error|
+      # no-op
+    end
+
     def perform(outbox_message, from: nil, to: nil, fs_client: FsEnvironment.fs_client, batch_size: 25)
       raise unless outbox_message.box.is_a?(Fs::Box)
       return unless outbox_message.box.syncable?
@@ -9,7 +16,7 @@ module Fs
       0.step do |k|
         received_messages = fs_api.fetch_received_messages(sent_message_id: outbox_message.metadata['fs_message_id'], page: k + 1, count: batch_size, from: from, to: to)
 
-        raise "No related messages!" if outbox_message.thread.messages.excluding(outbox_message).none? && received_messages['messages'].none? && outbox_message.delivered_at < 1.hour.ago
+        raise NoRelatedMessagesError if outbox_message.thread.messages.excluding(outbox_message).none? && received_messages['messages'].none? && outbox_message.delivered_at < 1.hour.ago
 
         received_messages['messages'].each do |received_message|
           ::Fs::DownloadReceivedMessageJob.perform_later(received_message['message_id'], box: outbox_message.box)
