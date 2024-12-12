9822c395855e1d2bb44c47a7ebd8b9f1287b1ff7
luciajanikova <19lucia99@gmail.com>
2024-12-12 08:20:40 +0100

    Add TODO

--- app/models/automation/condition.rb
+++ app/models/automation/condition.rb
@@ -120,6 +120,7 @@ module Automation
       if object.pdf?
         pdf_match?(object.content, value)
       elsif object.xml?
+        # TODO: fix encoding when saving content to DB
         Nokogiri::XML(object.content, nil, 'UTF-8').to_s.match?(value)
       end
     end
