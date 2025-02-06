f14d23fdde474cd9ff67428c9d4efa141cf4e126
luciajanikova <19lucia99@gmail.com>
2025-02-06 15:11:35 +0100

    Remove temporary jobs

--- db/migrate/20250204074342_create_inbox_tag.rb
+++ db/migrate/20250204074342_create_inbox_tag.rb
@@ -2,7 +2,6 @@ class CreateInboxTag < ActiveRecord::Migration[7.1]
   def up
     Tenant.find_each do |tenant|
       tenant.create_inbox_tag!(name: "Doručené", visible: false) unless tenant.inbox_tag
-      AddInboxTagToThreadsJob.set(job_context: :later).perform_later
     end
   end
 
