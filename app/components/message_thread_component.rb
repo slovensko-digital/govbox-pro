91497552b4096deffa51e4b12134006615464227
luciajanikova <19lucia99@gmail.com>
2025-02-28 11:57:32 +0100

    Add quick tags to thread detail

--- app/components/message_thread_component.rb
+++ app/components/message_thread_component.rb
@@ -1,10 +1,11 @@
 class MessageThreadComponent < ViewComponent::Base
-  def initialize(message_thread:, thread_tags:, thread_messages:, thread_last_message_draft_id:, non_quick_thread_tags: [], quick_tags: [])
+  def initialize(message_thread:, thread_tags:, thread_messages:, thread_last_message_draft_id:, non_quick_thread_tags: [], quick_tags: [], quick_tags_changes: nil)
     @message_thread = message_thread
     @thread_tags = thread_tags
     @thread_messages = thread_messages
     @thread_last_message_draft_id = thread_last_message_draft_id
     @non_quick_thread_tags = non_quick_thread_tags
     @quick_tags = quick_tags
+    @quick_tags_changes = quick_tags_changes
   end
 end
