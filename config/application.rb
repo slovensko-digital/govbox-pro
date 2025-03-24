68091de315c1f5fa544e077be25b99aca8cf6d56
luciajanikova <19lucia99@gmail.com>
2025-03-24 16:29:25 +0100

    Preserve succeeded good jobs for 1 day

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module GovboxPro
 
     config.good_job.enable_cron = true
     config.good_job.smaller_number_is_higher_priority = true
-    config.good_job.cleanup_preserved_jobs_before_seconds_ago = 0.minutes
+    config.good_job.cleanup_preserved_jobs_before_seconds_ago = 1.days
     config.good_job.cleanup_discarded_jobs = false
 
     if ENV['AUTO_SYNC_BOXES'] == "ON"
