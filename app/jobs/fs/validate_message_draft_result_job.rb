d227c816ac049ca0dd413022b1e06007721c2db8
Risko986 <richardlences0@gmail.com>
2025-04-22 13:13:06 +0200

    reverse unwanted changes

--- app/jobs/fs/validate_message_draft_result_job.rb
+++ app/jobs/fs/validate_message_draft_result_job.rb
@@ -13,18 +13,18 @@ class Fs::ValidateMessageDraftResultJob < ApplicationJob
     elsif [400, 422].include?(response[:status])
       message_draft.metadata[:status] = 'invalid'
     else
-      raise "Unexpected response status: #{response[:status]}"
+      raise RuntimeError.new("Unexpected response status: #{response[:status]}")
     end
 
     unless response[:body]['result'] == 'OK'
       message_draft.metadata[:validation_errors] = {
         result: response[:body]['result'],
-        errors: response[:body]['problems']&.select { |problem| problem['level'] == 'error' }&.map { |problem| problem['message'] },
-        warnings: response[:body]['problems']&.select { |problem| problem['level'] == 'warning' }&.map { |problem| problem['message'] }
+        errors: response[:body]['problems']&.select { |problem| problem['level'] == 'error' }&.map{ |problem| problem['message'] },
+        warnings: response[:body]['problems']&.select { |problem| problem['level'] == 'warning' }&.map{ |problem| problem['message'] },
       }
 
       diff = response[:body]['problems']&.select { |problem| problem['level'] == 'diff' }
-      Rails.logger.info("Message draft DIFF: #{diff.map { |problem| problem["message"] }.join(", ")}") if diff.any?
+      Rails.logger.info("Message draft DIFF: #{diff.map{ |problem| problem['message']}.join(', ')}") if diff.any?
 
       message_draft.add_cascading_tag(message_draft.tenant.submission_error_tag) if message_draft.metadata[:validation_errors][:errors].any? || message_draft.metadata[:validation_errors][:warnings].any?
     end
