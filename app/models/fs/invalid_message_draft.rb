538d6be2dcf7b8d038ffa6c27bf78a65727e0696
Michal Barla <michal.barla@gmail.com>
2025-04-26 21:04:59 +0200

    Add UnprocessableTag

--- app/models/fs/invalid_message_draft.rb
+++ app/models/fs/invalid_message_draft.rb
@@ -1,5 +1,28 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id                 :bigint           not null, primary key
+#  collapsed          :boolean          default(FALSE), not null
+#  delivered_at       :datetime         not null
+#  html_visualization :text
+#  metadata           :json
+#  outbox             :boolean          default(FALSE), not null
+#  read               :boolean          default(FALSE), not null
+#  recipient_name     :string
+#  replyable          :boolean          default(TRUE), not null
+#  sender_name        :string
+#  title              :string
+#  type               :string
+#  uuid               :uuid             not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  author_id          :bigint
+#  import_id          :bigint
+#  message_thread_id  :bigint           not null
+#
 class Fs::InvalidMessageDraft < Fs::MessageDraft
   after_create do
-    add_cascading_tag(thread.box.tenant.submission_error_tag!)
+    add_cascading_tag(thread.box.tenant.unprocessable_tag!)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_26_150801) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_26_185447) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -573,12 +573,12 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_26_150801) do
     t.bigint "owner_id"
     t.string "external_name"
     t.string "type", null: false
-    t.string "icon"
-    t.integer "tag_groups_count", default: 0, null: false
     t.enum "color", enum_type: "color"
+    t.integer "tag_groups_count", default: 0, null: false
+    t.string "icon"
     t.index ["owner_id"], name: "index_tags_on_owner_id"
-    t.index ["tenant_id", "type"], name: "signings_tags", unique: true, where: "((type)::text = ANY ((ARRAY['SignatureRequestedTag'::character varying, 'SignedTag'::character varying])::text[]))"
     t.index ["tenant_id", "type", "name"], name: "index_tags_on_tenant_id_and_type_and_name", unique: true
+    t.index ["tenant_id", "type"], name: "signings_tags", unique: true, where: "((type)::text = ANY (ARRAY[('SignatureRequestedTag'::character varying)::text, ('SignedTag'::character varying)::text]))"
     t.index ["tenant_id"], name: "index_tags_on_tenant_id"
   end
 
