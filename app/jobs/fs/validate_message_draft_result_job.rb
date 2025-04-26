e48a872abc69fe732d3dffdc242af15d32286a01
Jan Suchal <jan.suchal@gmail.com>
2025-04-26 09:48:08 +0200

    Add logging

--- app/jobs/fs/validate_message_draft_result_job.rb
+++ app/jobs/fs/validate_message_draft_result_job.rb
@@ -4,6 +4,7 @@ class Fs::ValidateMessageDraftResultJob < ApplicationJob
   after_discard do |job, exception|
     return unless exception.is_a?(ActiveJob::DeserializationError)
 
+    Rails.logger.warn("Deleting job #{job.job_id} due to #{exception.message}")
     GoodJob::Job.find_by(active_job_id: job.job_id).destroy
   end
 
