2539204420bb5ca27061c533f513ea58393a5dbd
luciajanikova <19lucia99@gmail.com>
2025-06-24 14:12:13 +0200

    Retry getting DeliveryReport after submission

--- app/jobs/fs/download_sent_message_related_messages_job.rb
+++ app/jobs/fs/download_sent_message_related_messages_job.rb
@@ -3,9 +3,14 @@ module Fs
     class MissingRelatedMessagesError < StandardError
     end
 
+    class TemporaryMissingRelatedMessagesError < StandardError
+    end
+
     retry_on MissingRelatedMessagesError, attempts: 1 do |_job, _error|
       # no-op
     end
+
+    retry_on TemporaryMissingRelatedMessagesError, wait: ->( executions ) { executions * 5.minutes }
 
     def perform(outbox_message, from: nil, to: nil, fs_client: FsEnvironment.fs_client, batch_size: 25)
       raise unless outbox_message.box.is_a?(Fs::Box)
@@ -20,7 +25,10 @@ module Fs
           received_messages = fs_api.fetch_received_messages(sent_message_id: outbox_message.metadata['fs_message_id'], page: k + 1, count: batch_size, from: from, to: to, obo: fs_api.obo_without_delegate)
         end
 
-        raise MissingRelatedMessagesError if outbox_message.thread.messages.excluding(outbox_message).none? && received_messages['messages'].none? && outbox_message.delivered_at < 1.hour.ago
+        if received_messages['messages'].none? && outbox_message.thread.messages.excluding(outbox_message).none?
+          raise MissingRelatedMessagesError if outbox_message.delivered_at < 1.hour.ago
+          raise TemporaryMissingRelatedMessagesError
+        end
 
         received_messages['messages'].each do |received_message|
           ::Fs::DownloadReceivedMessageJob.perform_later(received_message['message_id'], box: outbox_message.box)
