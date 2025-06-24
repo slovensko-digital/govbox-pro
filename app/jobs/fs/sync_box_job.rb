412782903405dff35474309326aafba27e538767
luciajanikova <19lucia99@gmail.com>
2025-06-24 07:47:02 +0200

    Update FS sync logic

--- app/jobs/fs/sync_box_job.rb
+++ app/jobs/fs/sync_box_job.rb
@@ -1,11 +1,27 @@
 module Fs
   class SyncBoxJob < ApplicationJob
-    def perform(box, from: Date.today - 1.week, to: Date.tomorrow)
+    def perform(box, from: Date.today - 1.day, to: Date.tomorrow, fs_client: FsEnvironment.fs_client, batch_size: 25)
       raise unless box.is_a?(Fs::Box)
       return unless box.syncable?
 
-      box.messages.outbox.not_drafts.find_each do |outbox_message|
-        DownloadSentMessageRelatedMessagesJob.perform_later(outbox_message, from: from, to: to)
+      fs_api = fs_client.api(api_connection: box.api_connection, box: box)
+
+      0.step do |k|
+        received_messages = fs_api.fetch_received_messages(page: k + 1, count: batch_size, from: from, to: to)
+
+        if received_messages['messages'].none?
+          received_messages = fs_api.fetch_received_messages(page: k + 1, count: batch_size, from: from, to: to, obo: fs_api.obo_without_delegate)
+        end
+
+        received_messages['messages'].each do |received_message|
+          related_outbox_message = box.messages.not_drafts.where("metadata ->> 'fs_message_id' = ?", received_message['sent_message_id']).first
+
+          next unless related_outbox_message
+
+          DownloadSentMessageRelatedMessagesJob.perform_later(related_outbox_message, from: from, to: to)
+        end
+
+        break if received_messages['messages'].size < batch_size
       end
     end
   end
