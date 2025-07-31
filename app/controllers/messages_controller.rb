21973bb7555ba8a64afe2a900dbcdf3c8e943469
luciajanikova <19lucia99@gmail.com>
2025-07-31 13:59:21 +0200

    Small messages controller fix

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,7 @@ class MessagesController < ApplicationController
   def show
     authorize @message
 
-    @mode = params[:mode].to_sym
+    @mode = params[:mode]&.to_sym
     @collapsed = params[:collapsed] == 'true'
     @message.update(read: true)
   end
