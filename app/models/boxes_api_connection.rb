526246bf049c14495d4f681ac29f3f5587c2cf98
luciajanikova <19lucia99@gmail.com>
2025-08-14 11:17:03 +0200

    Create BoxesApiConnections

---
+++ app/models/boxes_api_connection.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: boxes_api_connections
+#
+#  id                :bigint           not null, primary key
+#  settings          :jsonb
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  api_connection_id :bigint           not null
+#  box_id            :bigint           not null
+#
+class BoxesApiConnection < ApplicationRecord
+  belongs_to :box
+  belongs_to :api_connection
+end
