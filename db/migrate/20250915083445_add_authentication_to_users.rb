f528cee9d3521ad5ab427613eaeb765d11f4f81b
Richard Lences <richardlences0@gmail.com>
2025-09-23 16:22:37 +0200

    add require_http_auth to .env and remove username column

--- db/migrate/20250915083445_add_authentication_to_users.rb
+++ db/migrate/20250915083445_add_authentication_to_users.rb
@@ -1,6 +1,5 @@
 class AddAuthenticationToUsers < ActiveRecord::Migration[7.1]
   def change
-    add_column :users, :username, :string
     add_column :users, :password_digest, :string
   end
 end
--- db/migrate/20250922120000_remove_username_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUsernameFromUsers < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :users, :username, :string
-  end
-end
