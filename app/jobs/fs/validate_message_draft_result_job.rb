842b51444a155a245d92ec3c09d060d2e94fcc42
Risko986 <richardlences0@gmail.com>
2025-10-11 16:48:46 +0200

    undo unwanted changes

--- app/jobs/fs/validate_message_draft_result_job.rb
+++ app/jobs/fs/validate_message_draft_result_job.rb
@@ -9,18 +9,18 @@ class Fs::ValidateMessageDraftResultJob < ApplicationJob
     elsif [400, 422].include?(response[:status])
       message_draft.metadata[:status] = 'invalid'
     else
-      raise "Unexpected response status: #{response[:status]}"
+      raise RuntimeError.new("Unexpected response status: #{response[:status]}")
     end
 
-    errors = response[:body]['problems']&.select { |problem| problem['level'] == 'error' }&.map { |problem| problem['message'] } || []
-    warnings = response[:body]['problems']&.select { |problem| problem['level'] == 'warning' }&.map { |problem| problem['message'] } || []
-    diff = response[:body]['problems']&.select { |problem| problem['level'] == 'diff' }&.map { |problem| problem['message'] } || []
+    errors = response[:body]['problems']&.select { |problem| problem['level'] == 'error' }&.map{ |problem| problem['message'] } || []
+    warnings = response[:body]['problems']&.select { |problem| problem['level'] == 'warning' }&.map{ |problem| problem['message'] } || []
+    diff = response[:body]['problems']&.select { |problem| problem['level'] == 'diff' }&.map{ |problem| problem['message'] } || []
 
     result = if errors.none? && warnings.none? && diff.any?
-               'OK'
-             else
-               response[:body]['result']
-             end
+      'OK'
+    else
+      response[:body]['result']
+    end
 
     message_draft.metadata[:validation_errors] = {
       result: result,
@@ -29,7 +29,7 @@ class Fs::ValidateMessageDraftResultJob < ApplicationJob
       diff: diff
     }
 
-    # TODO: log occurence if diff.any?
+    # TODO log occurence if diff.any?
 
     message_draft.add_cascading_tag(message_draft.tenant.submission_error_tag) if errors.any? || warnings.any?
 
