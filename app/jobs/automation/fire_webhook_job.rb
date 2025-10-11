953cb3633e17eb468beb1addb487a0b1687e7c9c
Miroslav Hettes <mirrec@gmail.com>
2025-10-11 12:54:29 +0200

    Remove job test

--- app/jobs/automation/fire_webhook_job.rb
+++ app/jobs/automation/fire_webhook_job.rb
@@ -2,7 +2,7 @@ class Automation::FireWebhookJob < ApplicationJob
   queue_as :automation
   retry_on StandardError, wait: :polynomially_longer, attempts: 10
 
-  def perform(webhook, thing, event, timestamp, downloader: Faraday)
-    webhook.fire! thing, event, timestamp, downloader: downloader
+  def perform(webhook, thing, event, timestamp)
+    webhook.fire! thing, event, timestamp
   end
 end
