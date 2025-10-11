110560bf779ef3b42d52ea09f546a838dcdb518d
Risko986 <richardlences0@gmail.com>
2025-10-11 16:36:22 +0200

    discard jobs on deserialization error

--- app/jobs/fs/validate_message_draft_result_job.rb
+++ app/jobs/fs/validate_message_draft_result_job.rb
@@ -1,12 +1,5 @@
 class Fs::ValidateMessageDraftResultJob < ApplicationJob
-  discard_on ActiveJob::DeserializationError
-
-  after_discard do |job, exception|
-    return unless exception.is_a?(ActiveJob::DeserializationError)
-
-    Rails.logger.warn("Deleting job #{job.job_id} due to #{exception.message}")
-    GoodJob::Job.find_by(active_job_id: job.job_id).destroy
-  end
+  include DiscardOnDeserializationError
 
   def perform(message_draft, location_header, fs_client: FsEnvironment.fs_client)
     response = fs_client.api(box: message_draft.thread.box).get_location(location_header)
@@ -16,18 +9,18 @@ class Fs::ValidateMessageDraftResultJob < ApplicationJob
     elsif [400, 422].include?(response[:status])
       message_draft.metadata[:status] = 'invalid'
     else
-      raise RuntimeError.new("Unexpected response status: #{response[:status]}")
+      raise "Unexpected response status: #{response[:status]}"
     end
 
-    errors = response[:body]['problems']&.select { |problem| problem['level'] == 'error' }&.map{ |problem| problem['message'] } || []
-    warnings = response[:body]['problems']&.select { |problem| problem['level'] == 'warning' }&.map{ |problem| problem['message'] } || []
-    diff = response[:body]['problems']&.select { |problem| problem['level'] == 'diff' }&.map{ |problem| problem['message'] } || []
+    errors = response[:body]['problems']&.select { |problem| problem['level'] == 'error' }&.map { |problem| problem['message'] } || []
+    warnings = response[:body]['problems']&.select { |problem| problem['level'] == 'warning' }&.map { |problem| problem['message'] } || []
+    diff = response[:body]['problems']&.select { |problem| problem['level'] == 'diff' }&.map { |problem| problem['message'] } || []
 
     result = if errors.none? && warnings.none? && diff.any?
-      'OK'
-    else
-      response[:body]['result']
-    end
+               'OK'
+             else
+               response[:body]['result']
+             end
 
     message_draft.metadata[:validation_errors] = {
       result: result,
@@ -36,7 +29,7 @@ class Fs::ValidateMessageDraftResultJob < ApplicationJob
       diff: diff
     }
 
-    # TODO log occurence if diff.any?
+    # TODO: log occurence if diff.any?
 
     message_draft.add_cascading_tag(message_draft.tenant.submission_error_tag) if errors.any? || warnings.any?
 
