10b84f939a14a2f67b1456445111de36aa05f9ab
luciajanikova <19lucia99@gmail.com>
2025-10-13 11:31:40 +0200

    Update query in Fs::SyncAllBoxesJob

--- app/jobs/fs/sync_all_boxes_job.rb
+++ app/jobs/fs/sync_all_boxes_job.rb
@@ -2,7 +2,7 @@ module Fs
   class SyncAllBoxesJob < ApplicationJob
     def perform
       Fs::Box.where(syncable: true).find_each.with_index do |box, index|
-        box.boxes_api_connections.group_by { |boxes_api_connection| boxes_api_connection.settings_delegate_id }.each do |settings_delegate_id, boxes_api_connections|
+        box.boxes_api_connections.group_by(&:settings_delegate_id).each do |settings_delegate_id, boxes_api_connections|
           SyncBoxJob.set(wait: index*1.second).perform_later(box, api_connection: boxes_api_connections.first.api_connection)
         end
       end
