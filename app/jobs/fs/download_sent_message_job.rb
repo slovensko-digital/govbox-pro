4ec0c9dbadd3aa7e2f8179e74917f60fd6d95bc7
luciajanikova <19lucia99@gmail.com>
2025-10-22 13:04:10 +0200

    Download FS sent message with same API connection as was used for submission

--- app/jobs/fs/download_sent_message_job.rb
+++ app/jobs/fs/download_sent_message_job.rb
@@ -1,17 +1,19 @@
 module Fs
   class DownloadSentMessageJob < ApplicationJob
-    def perform(fs_message_id, box:, fs_client: FsEnvironment.fs_client)
-      raise unless box.is_a?(Fs::Box)
-      return unless box.syncable?
+    include Fs::JobHelper
 
-      return if box.messages.not_drafts.where("messages.metadata ->> 'fs_message_id' = ?", fs_message_id).any?
+    def perform(fs_message_id, message_draft:, fs_client: FsEnvironment.fs_client)
+      raise unless message_draft.box.is_a?(Fs::Box)
+      return unless message_draft.box.syncable?
+
+      return if message_draft.box.messages.not_drafts.where("messages.metadata ->> 'fs_message_id' = ?", fs_message_id).any?
 
       ActiveRecord::Base.transaction do
-        fs_api = fs_client.api(api_connection: box.api_connection, box: box)
+        fs_api = fs_client.api(api_connection: find_api_connection_for_outbox_message(message_draft), box: message_draft.box)
 
         raw_message = fs_api.fetch_sent_message(fs_message_id)
 
-        message = Fs::Message.create_outbox_message_with_thread!(raw_message, box: box)
+        message = Fs::Message.create_outbox_message_with_thread!(raw_message, box: message_draft.box)
 
         DownloadSentMessageRelatedMessagesJob.set(wait: 3.minutes).perform_later(message)
       end
--- app/jobs/fs/download_sent_message_related_messages_job.rb
+++ app/jobs/fs/download_sent_message_related_messages_job.rb
@@ -1,5 +1,7 @@
 module Fs
   class DownloadSentMessageRelatedMessagesJob < ApplicationJob
+    include Fs::JobHelper
+
     class MissingRelatedMessagesError < StandardError
     end
 
@@ -36,16 +38,6 @@ module Fs
 
         break if received_messages['messages'].size < batch_size
       end
-    end
-
-    private
-
-    def find_api_connection_for_outbox_message(outbox_message)
-      return outbox_message.box.api_connection if outbox_message.box.api_connections.count == 1
-
-      signed_by = outbox_message.form_object.tags.where(type: "SignedByTag")&.first&.owner
-      signers_api_connection = outbox_message.box.api_connections.find_by(owner: signed_by)
-      signers_api_connection if signed_by && signers_api_connection
     end
   end
 end
