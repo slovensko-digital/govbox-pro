358fb6b6fce0ab4de5421f6719753350db1f70bd
luciajanikova <19lucia99@gmail.com>
2025-11-20 16:16:27 +0100

    Rollback using HTML XSLT for FS message visualizations

--- app/helpers/fs/message_helper.rb
+++ app/helpers/fs/message_helper.rb
@@ -15,43 +15,13 @@ module Fs::MessageHelper
   def self.build_html_visualization_from_form(message)
     return unless message.form_object&.unsigned_content
 
-    xslt = if message.form_object.unsigned_content.size > 250.kilobytes
-             message.form&.xslt_txt
-           else
-             message.form&.xslt_html || message.form&.xslt_txt
-           end
+    xslt = message.form&.xslt_txt
 
-    raise 'Missing Fs::Form XSLT (HTML and TXT both unavailable)' unless xslt
+    raise 'Missing Fs::Form TXT XSLT' unless xslt
 
     template = Nokogiri::XSLT(xslt)
     transformed_content = template.transform(message.form_object.xml_unsigned_content).to_s
-
-    if xslt == message.form&.xslt_txt
-      transformed_content = ActionController::Base.helpers.simple_format(transformed_content)
-    else
-      forms_storage_url = ENV['FS_FORMS_STORAGE_API_URL']
-      form_path = "#{message.form.slug}/1.0/Content"
-      base_url = "#{forms_storage_url}/#{form_path}"
-
-      doc = Nokogiri::HTML::DocumentFragment.parse(transformed_content)
-
-      {
-        'script[src]' => 'src',
-        'link[rel="stylesheet"][href]' => 'href',
-        'img[src]' => 'src'
-      }.each do |selector, attr|
-        doc.css(selector).each do |el|
-          url = el[attr]
-          next unless url
-          next if url.start_with?('http', '//', 'data:')
-
-          normalized_path = url.gsub(%r{^\.\.?/}, '')
-          el[attr] = "#{base_url}/#{normalized_path}"
-        end
-      end
-
-      transformed_content = doc.to_html.html_safe
-    end
+    transformed_content = ActionController::Base.helpers.simple_format(transformed_content)
 
     ActionController::Base.new.render_to_string('fs/messages/_style', layout: false, locals: { message: message }) + transformed_content
   end
