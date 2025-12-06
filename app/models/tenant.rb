14c46c23daa636e6615433c433b786ba8b00504a
Jozef Steinhübl <contact@xhyrom.dev>
2025-12-06 14:13:52 +0100

    feat: add upvs feature flag

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -51,7 +51,7 @@ class Tenant < ApplicationRecord
   validates_presence_of :name
 
   AVAILABLE_FEATURE_FLAGS = [:audit_log, :archive, :api, :fs_sync]
-  ALL_FEATURE_FLAGS = [:audit_log, :archive, :api, :message_draft_import, :fs_api, :fs_sync]
+  ALL_FEATURE_FLAGS = [:audit_log, :archive, :api, :message_draft_import, :fs_api, :fs_sync, :upvs]
 
   PDF_SIGNATURE_FORMATS = %w[PAdES XAdES CAdES]
 
