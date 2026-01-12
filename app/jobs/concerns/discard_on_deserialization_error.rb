9b120978ec600a31bf9673cedbb98969628df874
luciajanikova <19lucia99@gmail.com>
2026-01-12 11:20:35 +0100

    Rollback changes in DiscardOnDeserializationError module

--- app/jobs/concerns/discard_on_deserialization_error.rb
+++ app/jobs/concerns/discard_on_deserialization_error.rb
@@ -5,7 +5,7 @@ module DiscardOnDeserializationError
     discard_on ActiveJob::DeserializationError
 
     after_discard do |job, exception|
-      next unless exception.is_a?(ActiveJob::DeserializationError)
+      return unless exception.is_a?(ActiveJob::DeserializationError)
 
       Rails.logger.warn("Deleting job #{job.job_id} due to #{exception.message}")
       GoodJob::Job.find_by(active_job_id: job.job_id).destroy
