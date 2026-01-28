e1cfc19db2c6fbdfd03ed38519e63ed5ba497e1b
luciajanikova <19lucia99@gmail.com>
2026-01-28 08:53:55 +0100

    Fix build

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,34 +1,36 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  # TODO move to tenant config
+if Rails.const_defined?(:Server)
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    # TODO move to tenant config
 
-  configure do |config|
-    # Raise errors in every environment instead of redirecting to the default error page.
-    config.failure_raise_out_environments = ['development', 'production', 'staging', 'test']
+    configure do |config|
+      # Raise errors in every environment instead of redirecting to the default error page.
+      config.failure_raise_out_environments = ['development', 'production', 'staging', 'test']
 
-    # Respond to saml, saml/callback, saml/metadata, saml/slo, and saml/spslo under path prefix.
-    config.path_prefix = '/auth'
+      # Respond to saml, saml/callback, saml/metadata, saml/slo, and saml/spslo under path prefix.
+      config.path_prefix = '/auth'
 
-    config.on_failure do |env|
-      SessionsController.action(:destroy).call(env)
+      config.on_failure do |env|
+        SessionsController.action(:destroy).call(env)
+      end
+
+      # Use default application logger.
+      config.logger = Rails.logger
     end
 
-    # Use default application logger.
-    config.logger = Rails.logger
+      provider :identity,
+             model: User,
+             fields: [:email],
+             enable_registration: false,
+             on_login: lambda { |env|
+               SessionsController.action(:login).call(env)
+             }
+
+    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
+      scope: 'email'
+    }
+
+    provider :microsoft_graph, ENV['AZURE_APPLICATION_CLIENT_ID'], ENV['AZURE_APPLICATION_CLIENT_SECRET']
+
+    provider :saml, UpvsEnvironment.sso_settings if UpvsEnvironment.sso_support?
   end
-
-  provider :identity,
-           model: User,
-           fields: [:email],
-           enable_registration: false,
-           on_login: lambda { |env|
-             SessionsController.action(:login).call(env)
-           }
-
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
-    scope: 'email'
-  }
-
-  provider :microsoft_graph, ENV['AZURE_APPLICATION_CLIENT_ID'], ENV['AZURE_APPLICATION_CLIENT_SECRET']
-
-  provider :saml, UpvsEnvironment.sso_settings if UpvsEnvironment.sso_support?
 end
