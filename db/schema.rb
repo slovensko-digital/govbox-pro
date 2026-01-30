9f48cec6a11e8ec14c56744d3f05c05c0e355e90
luciajanikova <19lucia99@gmail.com>
2026-01-30 18:18:23 +0100

    Add schema.rb changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_01_20_115648) do
+ActiveRecord::Schema[7.1].define(version: 2026_01_30_162220) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
