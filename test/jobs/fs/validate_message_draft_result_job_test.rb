083a3317542b603abb86e1e33177063024ea7b4f
luciajanikova <19lucia99@gmail.com>
2026-02-17 13:18:36 +0100

    Test setting internal errors on Fs::MessageDraft during validation

--- test/jobs/fs/validate_message_draft_result_job_test.rb
+++ test/jobs/fs/validate_message_draft_result_job_test.rb
@@ -117,4 +117,84 @@ class Fs::ValidateMessageDraftResultJobTest < ActiveJob::TestCase
       assert_not outbox_message.thread.tags.include?(outbox_message.tenant.signer_group.signature_requested_from_tag)
     end
   end
+
+  test "message is marked as invalid & signature is not requested if missing required attachment" do
+    outbox_message = messages(:fs_accountants_draft_vp_danv24)
+    url = "https://fsapi.test/submissions/#{outbox_message.id}"
+
+    fs_api = Minitest::Mock.new
+    fs_api.expect :get_location, {
+      :status => 200,
+      :body => {
+        'result' => 'OK'
+      }
+    },
+    [url]
+
+    FsEnvironment.fs_client.stub :api, fs_api do
+      Fs::ValidateMessageDraftResultJob.new.perform(outbox_message, url)
+      assert_equal 'invalid', outbox_message.metadata['status']
+      assert_equal 'OK', outbox_message.metadata['validation_errors']['result']
+      assert_equal [], outbox_message.metadata['validation_errors']['errors']
+      assert_equal ['Chýba požadovaná príloha pre VP_PRI_UA (minimum je 1).'], outbox_message.metadata['validation_errors']['internal_errors']
+
+      assert_not outbox_message.form_object.tags.include?(outbox_message.tenant.signer_group.signature_requested_from_tag)
+      assert_not outbox_message.thread.tags.include?(outbox_message.tenant.signer_group.signature_requested_from_tag)
+    end
+  end
+
+  test "message is marked as invalid & signature is not requested if attachment has invalid mimetype" do
+    outbox_message = messages(:fs_accountants_draft_vp_danv24)
+    outbox_message.objects.create!(name: 'priloha.cer', mimetype: 'application/pkix-cert', object_type: 'ATTACHMENT', uuid: SecureRandom.uuid)
+
+    url = "https://fsapi.test/submissions/#{outbox_message.id}"
+
+    fs_api = Minitest::Mock.new
+    fs_api.expect :get_location, {
+      :status => 200,
+      :body => {
+        'result' => 'OK'
+      }
+    },
+    [url]
+
+    FsEnvironment.fs_client.stub :api, fs_api do
+      Fs::ValidateMessageDraftResultJob.new.perform(outbox_message, url)
+      assert_equal 'invalid', outbox_message.metadata['status']
+      assert_equal 'OK', outbox_message.metadata['validation_errors']['result']
+      assert_equal [], outbox_message.metadata['validation_errors']['errors']
+      assert 'Nepovolený typ súboru: priloha.cer.'.in?(outbox_message.metadata['validation_errors']['internal_errors'])
+
+      assert_not outbox_message.form_object.tags.include?(outbox_message.tenant.signer_group.signature_requested_from_tag)
+      assert_not outbox_message.thread.tags.include?(outbox_message.tenant.signer_group.signature_requested_from_tag)
+    end
+  end
+
+  test "message is marked as invalid & signature is not requested if extra attachment" do
+    outbox_message = messages(:fs_accountants_draft_uzmujv14)
+    outbox_message.objects.create!(name: 'priloha1.pdf', mimetype: 'application/pdf', object_type: 'ATTACHMENT', uuid: SecureRandom.uuid)
+    outbox_message.objects.create!(name: 'priloha2.pdf', mimetype: 'application/pdf', object_type: 'ATTACHMENT', uuid: SecureRandom.uuid)
+
+    url = "https://fsapi.test/submissions/#{outbox_message.id}"
+
+    fs_api = Minitest::Mock.new
+    fs_api.expect :get_location, {
+      :status => 200,
+      :body => {
+        'result' => 'OK'
+      }
+    },
+    [url]
+
+    FsEnvironment.fs_client.stub :api, fs_api do
+      Fs::ValidateMessageDraftResultJob.new.perform(outbox_message, url)
+      assert_equal 'invalid', outbox_message.metadata['status']
+      assert_equal 'OK', outbox_message.metadata['validation_errors']['result']
+      assert_equal [], outbox_message.metadata['validation_errors']['errors']
+      assert 'Príliš veľa príloh pre UVPOD3_UA (maximum je 1).'.in?(outbox_message.metadata['validation_errors']['internal_errors'])
+
+      assert_not outbox_message.form_object.tags.include?(outbox_message.tenant.signer_group.signature_requested_from_tag)
+      assert_not outbox_message.thread.tags.include?(outbox_message.tenant.signer_group.signature_requested_from_tag)
+    end
+  end
 end
