7088dda8c94871c54c1f86ff4e6748269c5ad3ae
Jozef Steinhübl <contact@xhyrom.dev>
2026-02-23 21:40:14 +0100

    build: gem

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -17,7 +17,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   end
 
   provider :identity,
-           model: Identity,
            fields: [:email],
            enable_registration: false,
            on_login: lambda { |env|
