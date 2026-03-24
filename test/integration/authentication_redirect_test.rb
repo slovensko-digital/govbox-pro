f945fa58ab26eccaba7755d3888b4ec422b93d5d
Jozef Steinhübl <contact@xhyrom.dev>
2026-03-24 10:06:12 +0100

    test: make authentication redirect test integration test

--- test/integration/authentication_redirect_test.rb
+++ test/integration/authentication_redirect_test.rb
@@ -1,16 +1,11 @@
 require "test_helper"
 
-class MessagesAuthenticationRedirectTest < ActionController::TestCase
-  tests MessagesController
-
-  setup do
-    session[:login_expires_at] = 1.day.ago
-  end
+class AuthenticationRedirectTest < ActionDispatch::IntegrationTest
 
   test "stores navigational path while redirecting to login" do
     message = messages(:ssd_main_general_one)
 
-    get :show, params: { id: message.id }
+    get message_path(message)
 
     assert_redirected_to auth_path
     assert_equal message_path(message), session[:after_login_path]
@@ -19,25 +14,17 @@ class MessagesAuthenticationRedirectTest < ActionController::TestCase
   test "does not store export path while redirecting to login" do
     message = messages(:ssd_main_general_one)
 
-    get :export, params: { id: message.id }
+    get export_message_path(message)
 
     assert_redirected_to auth_path
     assert_nil session[:after_login_path]
-  end
-end
-
-class MessageObjectsAuthenticationRedirectTest < ActionController::TestCase
-  tests MessageObjectsController
-
-  setup do
-    session[:login_expires_at] = 1.day.ago
   end
 
   test "does not store attachment preview path while redirecting to login" do
     message = messages(:ssd_main_general_one)
     attachment = message_objects(:ssd_main_general_one_attachment)
 
-    get :show, params: { message_id: message.id, id: attachment.id }
+    get message_message_object_path(message, attachment)
 
     assert_redirected_to auth_path
     assert_nil session[:after_login_path]
