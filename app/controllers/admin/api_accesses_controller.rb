1bd02cc890ff70ac9ab67955408172827ef15a28
Jozef Steinhübl <contact@xhyrom.dev>
2026-03-28 12:06:13 +0100

    refactor: move api token public key validation to tenant

--- app/controllers/admin/api_accesses_controller.rb
+++ app/controllers/admin/api_accesses_controller.rb
@@ -6,22 +6,17 @@ class Admin::ApiAccessesController < ApplicationController
   def update
     authorize([:admin, :api_access])
 
-    if params[:delete_api_token_public_key] == "true" || api_access_params[:api_token_public_key].blank?
+    if api_access_params[:api_token_public_key].blank?
       Current.tenant.update(api_token_public_key: nil)
       redirect_to admin_tenant_api_access_path, notice: "Verejný kľúč bol úspešne odstránený"
       return
     end
 
-    validator = ApiTokenPublicKeyValidator.new(api_access_params[:api_token_public_key])
-
-    unless validator.valid?
-      Current.tenant.errors.add(:api_token_public_key, validator.error_message)
+    if Current.tenant.update(api_access_params)
+      redirect_to admin_tenant_api_access_path, notice: "Verejný kľúč bol úspešne aktualizovaný"
+    else
       render :show, status: :unprocessable_content
-      return
     end
-
-    Current.tenant.update(api_token_public_key: validator.sanitized_key)
-    redirect_to admin_tenant_api_access_path, notice: "Verejný kľúč bol úspešne aktualizovaný"
   end
 
   private
