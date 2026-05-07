21769810fc537bfdb773affaceeee90a85317d5f
Jozef Steinhübl <contact@xhyrom.dev>
2026-05-07 20:07:11 +0200

    fix: migration not reversible

--- db/migrate/20260507165513_make_email_optional_and_enforce_saml_or_email_on_users.rb
+++ db/migrate/20260507165513_make_email_optional_and_enforce_saml_or_email_on_users.rb
@@ -2,20 +2,9 @@ class MakeEmailOptionalAndEnforceSamlOrEmailOnUsers < ActiveRecord::Migration[7.
   def up
     change_column_null :users, :email, true
 
-    execute <<~SQL.squish
-      ALTER TABLE users
-        ADD CONSTRAINT email_or_saml_identifier_required
-        CHECK (email IS NOT NULL OR saml_identifier IS NOT NULL);
-    SQL
-  end
-
-  def down
-    execute <<~SQL.squish
-      ALTER TABLE users
-        DROP CONSTRAINT email_or_saml_identifier_required;
-    SQL
-
-    change_column_null :users, :email, false
+    add_check_constraint :users,
+                         "email IS NOT NULL OR saml_identifier IS NOT NULL",
+                         name: "email_or_saml_identifier_required"
   end
 end
 
