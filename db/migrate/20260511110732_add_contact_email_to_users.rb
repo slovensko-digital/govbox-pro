b01b4466f526ca2a195412ed548971edb643dca5
Jozef Steinhübl <contact@xhyrom.dev>
2026-05-15 18:09:53 +0200

    refactor: rename migration

--- db/migrate/20260511110732_add_contact_email_to_users.rb
+++ db/migrate/20260511110732_add_contact_email_to_users.rb
@@ -1,4 +1,4 @@
-class AddContantEmailToUsers < ActiveRecord::Migration[7.1]
+class AddContactEmailToUsers < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :contact_email, :string
   end
