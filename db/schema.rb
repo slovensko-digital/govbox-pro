a99a131fc08ec4c82ecc0fe9dd461f76e7a24828
Richard Lences <richard.lences@gmail.com>
2026-07-28 15:59:10 +0200

    Update schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_07_01_155715) do
+ActiveRecord::Schema[7.1].define(version: 2026_07_27_162058) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
