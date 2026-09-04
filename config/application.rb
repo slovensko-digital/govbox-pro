1a4efb8bd783db287a8f4ebfa4e190ab05429b2f
Tomáš Drga <tomasxdrga@gmail.com>
2026-09-04 10:47:36 +0200

    Enforce encrypted api_token_private_key

--- config/application.rb
+++ config/application.rb
@@ -31,8 +31,6 @@ module GovboxPro
     config.active_record.encryption.deterministic_key = ENV['ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY']
     config.active_record.encryption.key_derivation_salt = ENV['ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT']
 
-    config.active_record.encryption.support_unencrypted_data = true
-
     previous_primary_key = ENV['ACTIVE_RECORD_ENCRYPTION_PREVIOUS_PRIMARY_KEY']
     if previous_primary_key.present?
       previous_key_derivation_salt = ENV.fetch(
